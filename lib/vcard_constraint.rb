df6fc4110007abfd88e7b54c386264b52f6ba774
Harry Whelchel <harrywhelchel@gmail.com>
2016-04-17 17:04:00 -0400

    adds tested vcard constraint

--- lib/vcard_constraint.rb
+++ lib/vcard_constraint.rb
@@ -1,5 +1,15 @@
 class VcardConstraint
-  def matches?
-    false
+  MEDIA_TYPES = %w(text/vcard text/directory text/x-vcard text/directory;profile=vCard)
+
+  def matches?(request)
+    media_content_types(request).any? do |type|
+      MEDIA_TYPES.include?(type)
+    end
+  end
+
+  private
+
+  def media_content_types(request)
+    request.request_parameters.select { |key, value| key.match(/MediaContentType/) }.values
   end
 end
