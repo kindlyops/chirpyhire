840c62c03c8e28ff3ee96f8d7ade7f7997bd6b4f
Harry Whelchel <harrywhelchel@gmail.com>
2016-04-17 21:40:42 -0400

    format message

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -9,10 +9,10 @@ class Referral < ActiveRecord::Base
     Sms::Response.new do |r|
       r.Message "Awesome! Please copy and text to #{lead.first_name}:"
       r.Message "Hey #{lead.first_name}. My home care agency, \
-       #{organization.name}, regularly hires caregivers. They \
-       treat me very well and have great clients. I think you \
-       would be a great fit here. Text CARE to #{organization.phone_number} \
-       to learn about opportunities."
+#{organization.name}, regularly hires caregivers. They \
+treat me very well and have great clients. I think you \
+would be a great fit here. Text CARE to #{organization.phone_number} \
+to learn about opportunities."
     end
   end
 end
