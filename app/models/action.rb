e8342522da214402fb5e26c3a8f83a4e84f63d1c
Harry Whelchel <harry@lodestonetechnologies.co>
2016-05-09 11:06:28 -0400

    adds action association methods

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,3 +1,4 @@
 class Action < ActiveRecord::Base
   belongs_to :actionable, polymorphic: true
+  belongs_to :trigger
 end
--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -1,3 +1,4 @@
 class Trigger < ActiveRecord::Base
   belongs_to :rule
+  has_many :actions
 end
