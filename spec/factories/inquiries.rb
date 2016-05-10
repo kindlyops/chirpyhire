6f4095c1164c5fafb5f3220d738e6c0814afc775
Harry Whelchel <harry@lodestonetechnologies.co>
2016-05-10 12:14:09 -0400

    tests inquiry

--- spec/factories/inquiries.rb
+++ spec/factories/inquiries.rb
@@ -2,5 +2,13 @@ FactoryGirl.define do
   factory :inquiry do
     question
     message
+
+    trait :with_media_question do
+      association :question, format: Question.formats[:media]
+    end
+
+    trait :with_text_question do
+      association :question, format: Question.formats[:text]
+    end
   end
 end
--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Inquiry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#expects?" do
+    context "question expects media" do
+      let(:inquiry) { create(:inquiry, :with_media_question) }
+
+      context "message has media" do
+        let(:message) { create(:message, properties: { "MediaUrl0" => "path/to/image" }) }
+
+        it "is true" do
+          expect(inquiry.expects?(message)).to eq(true)
+        end
+      end
+
+      context "message does not have media" do
+        let(:message) { create(:message, properties: { "Body" => Faker::Lorem.word }) }
+
+        it "is false" do
+          expect(inquiry.expects?(message)).to eq(false)
+        end
+      end
+    end
+
+    context "question expects text" do
+      let(:inquiry) { create(:inquiry, :with_text_question) }
+
+      context "message has text" do
+        let(:message) { create(:message, properties: { "Body" => Faker::Lorem.word }) }
+
+        it "is true" do
+          expect(inquiry.expects?(message)).to eq(true)
+        end
+      end
+
+      context "message does not have text" do
+        let(:message) { create(:message, properties: { "MediaUrl0" => "path/to/image" }) }
+
+        it "is false" do
+          expect(inquiry.expects?(message)).to eq(false)
+        end
+      end
+    end
+  end
 end
