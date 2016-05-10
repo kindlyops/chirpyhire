9964c8924231c12ad6d4d0226e1f93d33c02b3fe
Harry Whelchel <harry@lodestonetechnologies.co>
2016-05-10 11:46:35 -0400

    tests assigning user attributes on accepting invitation

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,6 +3,41 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
 
   let(:account) { create(:account) }
+  let(:invite_params) do
+   { email: "bob@example.com",
+     user_attributes: {
+      organization: account.organization
+    }
+  }
+ end
+
+  describe ".accept_invitation!" do
+    context "with an invitable account with a user" do
+      let(:invitable) do
+        Account.invite!(invite_params, account)
+      end
+
+      context "with user attributes passed" do
+        let(:invitation_attributes) do
+          {
+            invitation_token: invitable.raw_invitation_token,
+            password: "password",
+            password_confirmation: "password",
+            user_attributes: {
+              first_name: "Bob",
+              last_name: "Bobson"
+            }
+          }
+        end
+
+        it "does not override the user's organization" do
+          expect {
+            Account.accept_invitation!(invitation_attributes)
+          }.not_to change{invitable.reload.user.organization.present?}
+        end
+      end
+    end
+  end
 
   describe "#role" do
     it "is admin by default" do
