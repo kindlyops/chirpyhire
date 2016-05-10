f472605b62b3f5bb69ea60c6033208fe32fee10d
Harry Whelchel <harry@lodestonetechnologies.co>
2016-05-10 11:20:56 -0400

    adds trigger and template controller specs

--- spec/controllers/candidates_controller_spec.rb
+++ spec/controllers/candidates_controller_spec.rb
@@ -1,49 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe CandidatesController, type: :controller do
-  context "not logged in" do
-    describe "#index" do
-      it "302s" do
-        get :index
-        expect(response.status).to eq(302)
-      end
-    end
+  let(:user) { create(:user, :with_account) }
+  let(:account) { user.account }
+  let(:organization) { user.organization }
+
+  before(:each) do
+    sign_in(account)
   end
 
-  context "logged in" do
-    let(:user) { create(:user, :with_account) }
-    let(:account) { user.account }
-    let(:organization) { user.organization }
-
-    before(:each) do
-      sign_in(account)
+  describe "#index" do
+    it "is OK" do
+      get :index
+      expect(response).to be_ok
     end
 
-    describe "#index" do
-      it "is OK" do
-        get :index
-        expect(response).to be_ok
+    context "with candidates" do
+      let!(:users) { create_list(:user, 3, organization: organization) }
+      let!(:candidates) do
+        3.times.map do |i|
+          create(:candidate, user: users[i-1])
+        end
       end
 
-      context "with candidates" do
-        let!(:users) { create_list(:user, 3, organization: organization) }
-        let!(:candidates) do
-          3.times.map do |i|
-            create(:candidate, user: users[i-1])
-          end
-        end
+      it "returns the organization's candidates" do
+        get :index
+        expect(assigns(:candidates)).to eq(candidates)
+      end
 
-        it "returns the organization's candidates" do
+      context "with other organizations" do
+        let!(:other_candidates) { create_list(:candidate, 2) }
+        it "does not return the other organization's candidates" do
           get :index
-          expect(assigns(:candidates)).to eq(candidates)
-        end
-
-        context "with other organizations" do
-          let!(:other_candidates) { create_list(:candidate, 2) }
-          it "does not return the other organization's candidates" do
-            get :index
-            expect(assigns(:candidates)).not_to include(other_candidates)
-          end
+          expect(assigns(:candidates)).not_to include(other_candidates)
         end
       end
     end
