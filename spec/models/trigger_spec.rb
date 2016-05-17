c2e57ddf274f8abf5b37603a001c66f4e10b7520
Harry Whelchel <harry@lodestonetechnologies.co>
2016-05-16 21:23:04 -0400

    tests trigger fire method

--- spec/models/trigger_spec.rb
+++ spec/models/trigger_spec.rb
@@ -13,10 +13,24 @@ RSpec.describe Trigger, type: :model do
     end
 
     context "with only observable type" do
-      let(:trigger) { create(:trigger, observable_type: "Subscription") }
+      let(:trigger) { create(:trigger, observable_type: "Candidate") }
 
       it "is the type" do
         expect(trigger.description).to eq(trigger.observable_type)
+      end
+    end
+  end
+
+  describe "#fire" do
+    let(:trigger) { create(:trigger) }
+    let(:user) { create(:user) }
+
+    context "with actions" do
+      let!(:action) { create(:action, trigger: trigger) }
+
+      it "performs the action" do
+        expect_any_instance_of(Action).to receive(:perform).with(user)
+        trigger.fire(user)
       end
     end
   end
