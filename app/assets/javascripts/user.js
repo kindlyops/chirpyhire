06644148c4d277344f35bfc79a4750df0daac8b7
Harry Whelchel <harry@lodestonetechnologies.co>
2016-06-01 10:27:50 -0400

    initial timeline changes

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -19,4 +19,18 @@ $(document).on("page:change", function() {
       $("form.edit_user input[name='authenticity_token']").replaceWith(authToken);
     });
   });
+
+  var $timeline_block = $('.timeline-block');
+  $timeline_block.each(function() {
+      if ($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
+          $(this).find('.timeline-point, .timeline-content').addClass('is-hidden');
+      }
+  });
+  $(window).on('scroll', function() {
+      $timeline_block.each(function() {
+          if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.timeline-point').hasClass('is-hidden')) {
+              $(this).find('.timeline-point, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
+          }
+      });
+  });
 });
--- app/decorators/message_decorator.rb
+++ app/decorators/message_decorator.rb
@@ -16,4 +16,21 @@ class MessageDecorator < Draper::Decorator
   def icon_class
     "fa-commenting"
   end
+
+  def friendly_day
+    day = created_at.strftime('%B %d')
+    if day == Date.today.strftime('%B %d')
+      "Today"
+    elsif day == Date.yesterday.strftime('%B %d')
+      "Yesterday"
+    end
+  end
+
+  def time_at
+    if friendly_day
+      "#{friendly_day} at #{created_at.strftime('%l:%M%P')}"
+    else
+      "#{created_at.strftime('%B %d')} at #{created_at.strftime('%l:%M%P')}"
+    end
+  end
 end
