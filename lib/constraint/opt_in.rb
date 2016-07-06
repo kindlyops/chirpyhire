2bc9221c1b5fa75e1de82cb1a5f1e47e108fb72e
Harry Whelchel <harrywhelchel@gmail.com>
2016-07-06 11:50:26 -0400

    forgives punctuation for optin and optout

--- lib/constraint/opt_in.rb
+++ lib/constraint/opt_in.rb
@@ -3,7 +3,7 @@ module Constraint
     OPT_IN_RESPONSES = %w(START YES)
 
     def matches?(request)
-      OPT_IN_RESPONSES.include?(body(request).strip.upcase)
+      OPT_IN_RESPONSES.include?(body(request).gsub(/[^a-z0-9\s]/i, '').strip.upcase)
     end
 
     private
