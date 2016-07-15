8ea65d2d063b5313c44346b6276f4475ccac3e4d
Harry Whelchel <harry@lodestonetechnologies.co>
2016-07-15 16:45:54 -0400

    removes column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715193506) do
+ActiveRecord::Schema.define(version: 20160715204520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 20160715193506) do
   create_table "candidates", force: :cascade do |t|
     t.integer  "user_id",                                    null: false
     t.string   "status",               default: "Potential", null: false
-    t.boolean  "subscribed",           default: false,       null: false
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
     t.integer  "candidate_persona_id",                       null: false
