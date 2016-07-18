7d43c7f162260f45a9ad0fc0b5d14fe0ccf8016a
Harry Whelchel <harry@lodestonetechnologies.co>
2016-07-18 18:40:03 -0400

    adds images in messages

--- app/models/media_instance.rb
+++ app/models/media_instance.rb
@@ -1,5 +1,6 @@
 class MediaInstance < ApplicationRecord
   belongs_to :message
+  URI_BASE = "https://api.twilio.com"
 
   IMAGE_TYPES = %w(image/jpeg image/gif image/png image/bmp)
 
@@ -9,5 +10,9 @@ class MediaInstance < ApplicationRecord
 
   def self.images
     where(content_type: IMAGE_TYPES)
+  end
+
+  def location
+    "#{URI_BASE}#{uri.split('.').first}"
   end
 end
