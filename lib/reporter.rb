4f4331d458f71f199602a2226b818bda740cdd94
Harry Whelchel <harry@lodestonetechnologies.co>
2016-07-21 15:30:55 -0400

    use reporter for both weekly and daily reports

---
+++ lib/reporter.rb
@@ -0,0 +1,22 @@
+class Reporter
+
+  def initialize(recipients, report_klass, period)
+    @recipients = recipients
+    @report_klass = report_klass
+    @period = period
+  end
+
+  def report
+    recipients.find_each do |recipient|
+      ReportMailer.send(period, report_for(recipient)).deliver_now
+    end
+  end
+
+  private
+
+  def report_for(recipient)
+    report_klass.new(recipient)
+  end
+
+  attr_reader :recipients, :report_klass, :period
+end
