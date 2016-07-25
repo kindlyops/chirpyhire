18c8cbab91d1cd879bd6272d1b2d6b50a2430a9a
Harry Whelchel <harry@lodestonetechnologies.co>
2016-07-25 11:59:16 -0400

    adds disable turbolinks caching directive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def disable_turbolinks_caching
+    content_for(:disable_turbolinks_caching) do
+      raw("<meta name='turbolinks-cache-control' content='no-cache'>")
+    end
+  end
 end
