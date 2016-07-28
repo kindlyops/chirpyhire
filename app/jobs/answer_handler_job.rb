1e8fa3ebe9948ea23545220c73120f65bc163377
Harry Whelchel <harry@lodestonetechnologies.co>
2016-07-28 09:43:21 -0400

    pass in message into answer handler

--- app/jobs/answer_handler_job.rb
+++ app/jobs/answer_handler_job.rb
@@ -2,6 +2,17 @@ class AnswerHandlerJob < ApplicationJob
   queue_as :default
 
   def perform(sender, inquiry, message_sid)
-    AnswerHandler.call(sender, inquiry, message_sid)
+    @sender = sender
+    message = MessageHandler.call(sender, organization.get_message(message_sid))
+
+    AnswerHandler.call(sender, inquiry, message)
+  end
+
+  private
+
+  attr_reader :sender
+
+  def organization
+    sender.organization
   end
 end
