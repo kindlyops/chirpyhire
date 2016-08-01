c9969d35227d59b60e43e8725ed51da76232dd19
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-01 16:53:51 -0400

    set up navigation event handler on ready instead of on turbolinks load

--- app/assets/javascripts/components/navigation.js
+++ app/assets/javascripts/components/navigation.js
@@ -1,23 +1,4 @@
-$(window).resize(function() {
-  var more = document.getElementById("js-navigation-more");
-  if ($(more).length > 0) {
-    var windowWidth = $(window).width();
-    var moreLeftSideToPageLeftSide = $(more).offset().left;
-    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
-
-    if (moreLeftSideToPageRightSide < 330) {
-      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
-    }
-
-    if (moreLeftSideToPageRightSide > 330) {
-    $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
-    }
-  }
-});
-
-$(document).on("turbolinks:load", function() {
+$(document).on("ready", function() {
   var menuToggle = $(".js-mobile-menu").unbind();
   $("#js-navigation-menu").removeClass("show");
 
