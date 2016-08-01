68afd87c826e7c0a106fe887b63efc4531aec975
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-01 15:28:16 -0400

    fixes account management spec

--- app/assets/javascripts/components/navigation.js
+++ app/assets/javascripts/components/navigation.js
@@ -18,10 +18,10 @@ $(window).resize(function() {
 });
 
 $(document).on("turbolinks:load", function() {
-  var menuToggle = $("#js-mobile-menu").unbind();
+  var menuToggle = $(".js-mobile-menu").unbind();
   $("#js-navigation-menu").removeClass("show");
 
-  $(document).on("click", "#js-mobile-menu", function(e) {
+  $(document).on("click", ".js-mobile-menu", function(e) {
     e.preventDefault();
     $("#js-navigation-menu").slideToggle(function(){
       if($("#js-navigation-menu").is(":hidden")) {
