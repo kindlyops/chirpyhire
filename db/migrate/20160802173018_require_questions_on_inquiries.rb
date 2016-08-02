3c5c1a7227176e12bdfad3a837653ee953461a3d
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-02 13:31:16 -0400

    require question

---
+++ db/migrate/20160802173018_require_questions_on_inquiries.rb
@@ -0,0 +1,5 @@
+class RequireQuestionsOnInquiries < ActiveRecord::Migration[5.0]
+  def change
+    change_column :inquiries, :question_id, :integer, null: false, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802150123) do
+ActiveRecord::Schema.define(version: 20160802173018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 20160802150123) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "message_id",  null: false
-    t.integer  "question_id"
+    t.integer  "question_id", null: false
     t.index ["message_id"], name: "index_inquiries_on_message_id", using: :btree
     t.index ["question_id"], name: "index_inquiries_on_question_id", using: :btree
   end
