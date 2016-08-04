6250b45d8e8220a9727695fd0e7ffa452d05f9fa
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-04 13:20:55 -0400

    test that options are sorted in memory

--- spec/models/choice_question_spec.rb
+++ spec/models/choice_question_spec.rb
@@ -1,37 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ChoiceQuestion, type: :model do
+  let!(:organization) { create(:organization) }
+  let!(:message) { create(:message, body: "A) ") }
+  let(:survey) { create(:survey) }
+
+  let(:choice_hash) do
+    {
+      choice_option: "Live-in",
+      child_class: "choice"
+    }
+  end
+
+  let!(:choice_question) { create(:choice_question, text: "What is your availability?", survey: survey) }
+  let!(:options) do
+    [
+      choice_question.choice_question_options.create(letter: "a", text: "Live-in"),
+      choice_question.choice_question_options.create(letter: "b", text: "Hourly"),
+      choice_question.choice_question_options.create(letter: "c", text: "Both")
+    ]
+  end
+
   describe ".extract" do
-    let!(:organization) { create(:organization) }
-    let!(:question) { create(:choice_question, survey: organization.create_survey) }
-    let!(:message) { create(:message, body: "A) ") }
-
-    let(:choice_hash) do
-      {
-        choice_option: "Live-in",
-        child_class: "choice"
-      }
-    end
-
-    before(:each) do
-      question.choice_question_options.create(letter: "a", text: "Live-in")
-    end
-
     it "returns a choice hash" do
-      expect(ChoiceQuestion.extract(message, question)).to eq(choice_hash)
+      expect(ChoiceQuestion.extract(message, choice_question)).to eq(choice_hash)
     end
   end
 
-  let(:survey) { create(:survey) }
+
+  describe "#in_memory_sorted_options" do
+    it "sorts the options by letter" do
+      expect(choice_question.in_memory_sorted_options.map(&:letter)).to eq(%w(a b c))
+    end
+  end
 
   describe "#question" do
-    let(:choice_question) { create(:choice_question, text: "What is your availability?", survey: survey) }
-    before(:each) do
-      choice_question.choice_question_options.create(letter: "a", text: "Live-in")
-      choice_question.choice_question_options.create(letter: "b", text: "Hourly")
-      choice_question.choice_question_options.create(letter: "c", text: "Both")
-    end
-
     let(:question) do
       <<-question
 What is your availability?
