2da7b5d7afb248fd0564c9d5bcedf085f9325b68
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-08 09:22:29 -0400

    adds invitation link

--- spec/support/features/invitation_helpers.rb
+++ spec/support/features/invitation_helpers.rb
@@ -1,7 +1,9 @@
 module Features
   module InvitationHelpers
     def send_invitation_to(email)
-      visit new_account_invitation_path
+      visit root_path
+      find("#desktop-settings", match: :first).trigger('click')
+      click_link("Invite an Admin")
 
       fill_in "Email", with: email
       click_button "Invite"
