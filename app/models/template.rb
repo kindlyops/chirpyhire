f571f9222cd5bf8144323a83a510ca7009c48e32
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-09 17:18:29 -0400

    validate presence of body and name on templates [skip ci]

--- app/models/template.rb
+++ app/models/template.rb
@@ -4,6 +4,8 @@ class Template < ApplicationRecord
   has_one :survey
   belongs_to :actionable, foreign_key: :actionable_id, class_name: "TemplateActionable", inverse_of: :template
 
+  validates_presence_of :body, :name
+
   def perform(user)
     message = user.receive_message(body: body)
     notifications.create(message: message)
