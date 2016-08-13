a38f35ba1b1601ab64503dab1a466d772bfcc537
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-13 17:35:39 -0400

    adds tos and pp to sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809173848) do
+ActiveRecord::Schema.define(version: 20160813212249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160809173848) do
     t.string   "invited_by_type"
     t.integer  "invited_by_id"
     t.integer  "invitations_count",      default: 0
+    t.boolean  "agreed_to_terms",        default: false, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_accounts_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_accounts_on_invitations_count", using: :btree
