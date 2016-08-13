985f8f8055d3e6c50a60bdb7876ff288b88e3694
Harry Whelchel <harry@lodestonetechnologies.co>
2016-08-13 18:10:52 -0400

    use sequences in template factories

--- spec/factories/templates.rb
+++ spec/factories/templates.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :template do
     organization
-    name { Faker::Lorem.words(rand(1..3)).join }
-    body { Faker::Lorem.paragraph(rand(1..5)) }
+    sequence(:name) { |i| "#{Faker::Lorem.words(rand(1..3)).join}#{i}" }
+    sequence(:body) { |i| "#{Faker::Lorem.paragraph(rand(1..5))}#{i}" }
   end
 end
