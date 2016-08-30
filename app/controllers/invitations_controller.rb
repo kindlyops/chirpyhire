9463fef2549877ca5adf24ff0c3c4683886f56ba
Harry Whelchel <harry@chirpyhire.com>
2016-08-30 18:26:03 -0400

    fix survey_path method

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -19,7 +19,7 @@ class InvitationsController < Devise::InvitationsController
   end
 
   def after_invite_path_for(inviter)
-    survey_path(inviter.survey)
+    survey_path
   end
 
   def organization
