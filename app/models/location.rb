dbefde4088b882c2eb9011b05299588b42858b46
Harry Whelchel <harry@chirpyhire.com>
2016-08-30 22:31:56 -0400

    default is false

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ApplicationRecord
   belongs_to :organization
 
-  validates_length_of :state, is: 2, allow_blank: false
-  validates_length_of :state_code, is: 2, allow_blank: false
+  validates_length_of :state, is: 2
+  validates_length_of :state_code, is: 2
 end
