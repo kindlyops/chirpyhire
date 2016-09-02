2404c3775dd6d39f2b42e01b21aa80dce4d85a5e
Harry Whelchel <harry@chirpyhire.com>
2016-09-02 09:17:08 -0400

    after creating stripe subscription, kicks off a job to advance the survey

--- lib/payment/job/process_subscription.rb
+++ lib/payment/job/process_subscription.rb
@@ -1,6 +1,7 @@
 class Payment::Job::ProcessSubscription < ApplicationJob
   def perform(subscription, email)
     Payment::Subscriptions::Process.call(subscription, email)
+    SurveyAdvancer.call(subscription.organization)
   end
 end
 
---
+++ spec/lib/payment/job/process_subscription_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Payment::Job::ProcessSubscription do
+  let(:subscription) { create(:subscription) }
+  let(:email) { Faker::Internet.email }
+
+  describe "#perform" do
+    it "calls Process Service" do
+      expect(Payment::Subscriptions::Process).to receive(:call).with(subscription, email)
+      Payment::Job::ProcessSubscription.perform_now(subscription, email)
+    end
+
+    it "calls the SurveyAdvancer service" do
+      allow(Payment::Subscriptions::Process).to receive(:call).with(subscription, email)
+      expect(SurveyAdvancer).to receive(:call).with(subscription.organization)
+      Payment::Job::ProcessSubscription.perform_now(subscription, email)
+    end
+  end
+end
