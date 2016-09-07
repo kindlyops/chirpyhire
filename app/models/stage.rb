54218cc82ee6e370ac252a8354a5e44a1c2b1e0d
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-07 06:10:08 -0400

    Back populate with default stages for new organizations

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,3 +1,12 @@
 class Stage < ApplicationRecord
   belongs_to :organization
+
+  def self.defaults(organization_id:)
+    [
+      Stage.new(organization_id: organization_id, name: "Potential", order: 1),
+      Stage.new(organization_id: organization_id, name: "Qualified", order: 2),
+      Stage.new(organization_id: organization_id, name: "Bad Fit", order: 3),
+      Stage.new(organization_id: organization_id, name: "Hired", order: 4)
+    ]
+  end
 end
