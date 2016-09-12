dc8a3044e67202520da2fa799d95171c854ca140
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-12 16:59:51 -0400

    Add back default filter

--- app/controllers/maps/candidates_controller.rb
+++ app/controllers/maps/candidates_controller.rb
@@ -2,6 +2,8 @@ class Maps::CandidatesController < ApplicationController
   skip_after_action :verify_policy_scoped
   skip_after_action :verify_authorized
 
+  DEFAULT_FILTER = "Qualified"
+
   def show
     determine_status
 
