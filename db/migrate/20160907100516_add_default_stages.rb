70a778a622ada179c2f70002eb86764fbf5d140e
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-12 10:08:32 -0400

    Delete db update from migration

--- db/migrate/20160907100516_add_default_stages.rb
+++
@@ -1,7 +0,0 @@
-class AddDefaultStages < ActiveRecord::Migration[5.0]
-  def change
-    Organization.ids.each do |id|
-      Stage.defaults(organization_id: id).each { |stage| stage.save }
-    end
-  end
-end
