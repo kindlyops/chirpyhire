5cecd0a854d60398b9cc6ce1b75d2504c1bc0bd0
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-14 10:57:46 -0400

    Improve messaging for undeletable stages

--- app/decorators/stage_decorator.rb
+++ app/decorators/stage_decorator.rb
@@ -1,9 +1,18 @@
 class StageDecorator < Draper::Decorator
   delegate_all
+  
   def list_item
     delete_button_disabled_class = if candidates.any? then 'disabled' else '' end
     delete_button_tipsy_class = if candidates.any? then 'tipsy-needed-w' else '' end
-    delete_button_title = if candidates.any? then 'Please remove candidates from stage to delete' else 'Delete Stage' end
+
+    delete_button_title = 
+      if default_stage_mapping.present? 
+        then 'This stage cannot be deleted' 
+      elsif candidates.any? 
+        then 'Please remove candidates from stage to delete' 
+      else 'Delete Stage' 
+      end
+
     "<span class=order>#{order}.</span> #{name}\
      <button class='delete-stage #{delete_button_tipsy_class} #{delete_button_disabled_class}' title='#{delete_button_title}' data-id='#{id}'>\
        <i class='fa fa-trash-o'></i>\
