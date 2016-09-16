246c787ee4c1ff2e1e6ca1de55f580a2ce05eb9e
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-16 12:17:22 -0400

    Add rescue logic for when twilio message not found

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -8,17 +8,22 @@ class MessageHandler
     existing_message = sender.messages.find_by(sid: message_sid)
     return existing_message if existing_message.present?
 
-    external_message.media.each do |media_instance|
-      message.media_instances.new(
-        content_type: media_instance.content_type,
-        sid: media_instance.sid,
-        uri: media_instance.uri
-      )
-    end
+    begin
+      external_message.media.each do |media_instance|
+        message.media_instances.new(
+          content_type: media_instance.content_type,
+          sid: media_instance.sid,
+          uri: media_instance.uri
+        )
+      end
 
-    message.save
-    Threader.new(message).call
-    message
+      message.save
+      Threader.new(message).call
+      message
+    rescue Twilio::REST::RequestError => e
+      Rollbar.log(e, "Twilio message not found", { organization_id: @sender.organization_id })
+      MessageHandlerJob.perform_in(30.seconds, @sender, @message_sid)
+    end
   end
 
   def initialize(sender, message_sid)
