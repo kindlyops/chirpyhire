7d5129818bc0b8c09b0354c417386a9e9c3d5afb
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-16 15:02:48 -0400

    Review feedback

--- app/jobs/message_handler_job.rb
+++ app/jobs/message_handler_job.rb
@@ -1,5 +1,5 @@
 class MessageHandlerJob < ApplicationJob
-  def perform(sender, message_sid)
-    MessageHandler.new(sender, message_sid).call
+  def perform(sender, message_sid, retries: MessageHandler::DEFAULT_RETRIES)
+    MessageHandler.new(sender, message_sid, retries: retries).call
   end
 end
--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -1,33 +1,40 @@
 class MessageHandler
-
-  def initialize(sender, message_sid)
+  DEFAULT_RETRIES = 3
+  def initialize(sender, message_sid, retries: DEFAULT_RETRIES)
     @sender = sender
     @message_sid = message_sid
+    @retries = retries
   end
 
   def call
     existing_message = sender.messages.find_by(sid: message_sid)
-    return existing_message if existing_message.present?
-
-    begin
-      external_message.media.each do |media_instance|
-        message.media_instances.new(
-          content_type: media_instance.content_type,
-          sid: media_instance.sid,
-          uri: media_instance.uri
-        )
-      end
-
-      message.save
-      Threader.new(message).call
-      message
-    rescue Twilio::REST::RequestError => e
-      Rollbar.log(e, "Twilio message not found", { organization_id: @sender.organization_id })
-      MessageHandlerJob.perform_later(@sender, @message_sid)
-    end
+    return existing_message if existing_message.present? else handle_message
   end
 
   private
+
+  def handle_message
+    external_message.media.each do |media_instance|
+      message.media_instances.new(
+        content_type: media_instance.content_type,
+        sid: media_instance.sid,
+        uri: media_instance.uri
+      )
+    end
+
+    message.save
+    Threader.new(message).call
+    message
+
+  rescue Twilio::REST::RequestError => e
+    retries_remaining = @retries - 1
+    Rollbar.log(e, "Twilio message not found", { organization_id: @sender.organization_id, retries_remaining: retries_remaining })
+    if retries_remaining > 0
+      MessageHandlerJob.set(wait: 30.seconds).perform_later(@sender, @message_sid, retries_remaining)
+    else
+      raise
+    end
+  end
 
   def external_message
     organization.get_message(message_sid)
