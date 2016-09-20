066eaa541c52adc9140cfb8e8b363a9a9b1db54f
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-20 13:14:44 -0400

    back to spaces

--- app/assets/javascripts/sortable.js
+++ app/assets/javascripts/sortable.js
@@ -1,10 +1,10 @@
 App.Sortable = {
-	onMoveUpdateNumber: function(e) {
-		var draggedDiv = $(e.dragged).find(".sortable-number")[0],
-			relatedDiv = $(e.related).find(".sortable-number")[0],
-			draggedNumber = draggedDiv.textContent;
-		draggedDiv.textContent = relatedDiv.textContent;
-		relatedDiv.textContent = draggedNumber;
-	},
-	handle: ".drag-and-drop"
+  onMoveUpdateNumber: function(e) {
+    var draggedDiv = $(e.dragged).find(".sortable-number")[0],
+      relatedDiv = $(e.related).find(".sortable-number")[0],
+      draggedNumber = draggedDiv.textContent;
+    draggedDiv.textContent = relatedDiv.textContent;
+    relatedDiv.textContent = draggedNumber;
+  },
+  handle: ".drag-and-drop"
 }
--- app/assets/javascripts/stages.js
+++ app/assets/javascripts/stages.js
@@ -1,24 +1,24 @@
 $(document).on("turbolinks:load", function() {
-	var wrapper = $(".stages-wrapper"),
-		setupSorting = function() {
-			if(wrapper.find(".stage").length) {
-				var stagesList = wrapper.find(".stages"),
-					reorderForm = wrapper.find(".reorder-form"),
-					stagesListElement = stagesList[0];
-				new Sortable(stagesListElement, {
-					handle: App.Sortable.handle,
-					animation: 125,
-					chosenClass: "sortable-chosen",
-					ghostClass: "sortable-ghost",
-					onMove: App.Sortable.onMoveUpdateNumber,
-					onUpdate: function(e) {
-						stagesList.find(".stage").each(function(i, stageItem) {
-							reorderForm.find("input.hidden-order[name='" + $(stageItem).attr("data-id") + "']").val(i + 1);
-						});
-					}
-				});
-			};
-		};
+  var wrapper = $(".stages-wrapper"),
+    setupSorting = function() {
+      if(wrapper.find(".stage").length) {
+        var stagesList = wrapper.find(".stages"),
+          reorderForm = wrapper.find(".reorder-form"),
+          stagesListElement = stagesList[0];
+        new Sortable(stagesListElement, {
+          handle: App.Sortable.handle,
+          animation: 125,
+          chosenClass: "sortable-chosen",
+          ghostClass: "sortable-ghost",
+          onMove: App.Sortable.onMoveUpdateNumber,
+          onUpdate: function(e) {
+            stagesList.find(".stage").each(function(i, stageItem) {
+              reorderForm.find("input.hidden-order[name='" + $(stageItem).attr("data-id") + "']").val(i + 1);
+            });
+          }
+        });
+      };
+    };
 
-	setupSorting();
+  setupSorting();
 });
