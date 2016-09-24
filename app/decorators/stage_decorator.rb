29c9a59a674e58444690445bcad0df1065a26dda
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-23 21:57:46 -0400

    Remove edit and delete buttons for standard stages

--- app/decorators/stage_decorator.rb
+++ app/decorators/stage_decorator.rb
@@ -1,37 +1,3 @@
 class StageDecorator < Draper::Decorator
   delegate_all
-
-  def delete_button_class
-    return '' if StagePolicy.deletable?(self)
-    'disabled'
-  end
-
-  def modify_button_class
-    return '' if StagePolicy.deletable?(self)
-    'disabled'
-  end
-
-  def modify_button_title
-    if !StagePolicy.updatable?(self)
-      'This stage cannot be edited'
-    else
-      'Modify stage'
-    end
-  end
-
-  def delete_button_title
-    if standard_stage_mapping.present?
-      'This stage cannot be deleted'
-    elsif candidates.any?
-      'Please remove candidates from stage to delete'
-    else
-      'Delete Stage'
-    end
-  end
-
-  private
-
-  def tooltip_needed
-    candidates.any? || standard_stage_mapping.present?
-  end
 end
