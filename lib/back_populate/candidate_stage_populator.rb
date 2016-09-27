9a6fe366103d12715e7f9d635e1d877fab3308ad
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-27 14:56:22 -0400

    rubocop

--- lib/back_populate/candidate_stage_populator.rb
+++ lib/back_populate/candidate_stage_populator.rb
@@ -2,15 +2,17 @@ class BackPopulate::CandidateStagePopulator
   def self.populate(candidate)
     return unless candidate.stage_id.blank?
 
-    status_stage_map = {
+    status_stage_map(candidate).each do |status, stage|
+      candidate.update!(stage_id: stage.id) if candidate.status == status
+    end
+  end
+
+  def self.status_stage_map(candidate)
+    {
       'Potential' => candidate.organization.potential_stage,
       'Bad Fit' => candidate.organization.bad_fit_stage,
       'Qualified' => candidate.organization.qualified_stage,
       'Hired' => candidate.organization.hired_stage,
     }
-
-    status_stage_map.each do |status, stage|
-      candidate.update!(stage_id: stage.id) if candidate.status == status
-    end
   end
 end
