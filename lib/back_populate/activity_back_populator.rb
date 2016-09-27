2abfd03e3026c6fbc8968cc8e942d005a61c7ecd
johnwhelchel <john.l.whelchel@gmail.com>
2016-09-27 12:56:32 -0400

    Add code for back populating activity table

---
+++ lib/back_populate/activity_back_populator.rb
@@ -0,0 +1,27 @@
+class BackPopulate::ActivityBackPopulator
+  def self.populate(candidate)
+    activities = candidate.activities
+    activities.each do |activity|
+      update_activity(activity, candidate.organization)
+    end
+  end
+
+  private_class_method
+
+  def self.update_activity(activity, organization)
+    return if activity.properties.key?('stage_id')
+
+    status_stage_map = {
+      'Potential' => organization.potential_stage,
+      'Bad Fit' => organization.bad_fit_stage,
+      'Qualified' => organization.qualified_stage,
+      'Hired' => organization.hired_stage,
+    }
+
+    status = activity.properties['status']
+    if status.present?
+      activity.properties['stage_id'] = status_stage_map[status].id
+      activity.save!
+    end
+  end
+end
