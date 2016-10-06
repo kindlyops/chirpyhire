e430aaf241904cb21265d0f8a838fad31d173ebe
johnwhelchel <john.l.whelchel@gmail.com>
2016-10-06 15:50:10 -0400

    add index

--- db/migrate/20160927203623_add_nickname_to_candidate.rb
+++ db/migrate/20160927203623_add_nickname_to_candidate.rb
@@ -1,5 +1,6 @@
 class AddNicknameToCandidate < ActiveRecord::Migration[5.0]
   def change
     add_column :candidates, :nickname, :string
+    add_index :candidates, :nickname, unique: true
   end
 end
