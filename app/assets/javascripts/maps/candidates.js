714353f60c82291e1c2c9986eb1e19bab314ef38
johnwhelchel <john.l.whelchel@gmail.com>
2016-10-21 01:02:40 -0400

    First go at toggleable maps

--- app/assets/javascripts/maps/candidates.js
+++ app/assets/javascripts/maps/candidates.js
@@ -1,50 +1,19 @@
 $(document).on("turbolinks:load", function() {
   if($(".maps-candidates #map").length) {
     var dropdownStageSelector = ".maps-candidates .dropdown select",
-      isCurrentStageFunc = function(currentStageId) {
-        return function(stage) {
-          return stage.id.toString() === currentStageId
-        }
-      };
+      currentStageId = $(dropdownStageSelector).val(),
+      mapType = 'address',
+      stages,
+      map,
+      addressLayers = [],
+      zipcodeLayers = [];
 
-    function watchSelect(map, stages) {
-      $(document).on("change", dropdownStageSelector, function(event) {
-        var currentStageId = this.value,
-          isCurrentStage = isCurrentStageFunc(currentStageId)
-          currentStageName = 
-            stages.filter(isCurrentStage)[0].name,
-          unselectedStages = _.reject(stages, isCurrentStage);
-
-        map.popup.remove();
-
-        _.each(unselectedStages, function(stage) {
-          getOrderedLayerIds(stage.name).forEach(function(layer_id) {
-            map.setLayoutProperty(layer_id, 'visibility', 'none');
-          });
-        });
-        getOrderedLayerIds(currentStageName).forEach(function(layer_id) {
-          map.setLayoutProperty(layer_id, 'visibility', 'visible');
-        });
+    function init() {
+      $.get({
+        url: "/candidates.geojson",
+        dataType: "json",
+        success: initMap.bind(this)
       });
-    }
-
-    function watchCardLink(stages) {
-      $(document).on("click", ".maps-candidates .candidates-cards", function(event) {
-        var url = 
-          $(this).attr("href") 
-          + "?stage_name=" 
-          + stages.filter(isCurrentStageFunc($(dropdownStageSelector).val()))[0].name;
-        event.preventDefault();
-        Turbolinks.visit(url);
-      });
-    }
-
-    function getOrderedLayerIds(stage_name) {
-      return [
-        stage_name + "_address",
-        stage_name + "_zipcode",
-        stage_name + "_zipcode_hover"
-      ]
     }
 
     function initMap(candidatesGeoData) {
@@ -52,12 +21,11 @@ $(document).on("turbolinks:load", function() {
         zipcodeSourceId = "candidates_zipcode",
         center =  $("#map").data("center"),
         zoom = $("#map").data("zoom"),
-        stages = candidatesGeoData.stages,
         layers, sources,
         addressSource, zipcodeSource,
-        addressLayers, zipcodeLayers,
         nonHoverLayers;
 
+      stages = candidatesGeoData.stages;
       addressSource = {
         id: addressSourceId,
         type: "geojson",
@@ -73,41 +41,47 @@ $(document).on("turbolinks:load", function() {
       sources = [addressSource, zipcodeSource];
 
       addressLayers = _.map(stages, function(stage) {
-        layerIds = getOrderedLayerIds(stage.name);
+        var layerIds = getLayerIds(stage.name),
+          visible = $(dropdownStageSelector).val() === stage.id.toString()
+            && mapType == 'address';
         return {
-          "id": layerIds[0],
+          "id": layerIds.address,
           "type": "symbol",
           "source": addressSourceId,
           "layout": {
             "icon-image": App.MapsCommon.icon,
-            "visibility": $(dropdownStageSelector).val() === stage.id.toString() ? 'visible' : 'none'
+            "visibility": visible ? 'visible' : 'none'
           },
           "filter": ["==", "stage_name", stage.name]
         };
       });
       
       zipcodeLayersInfo = _.map(stages, function(stage) {
-        layerIds = getOrderedLayerIds(stage.name);
-        standardLayer = {
-          "id": layerIds[1],
-          "type": "fill",
-          "source": zipcodeSourceId,
-          "paint": App.MapsCommon.paintFill,
-          "layout": {
-            "visibility": $(dropdownStageSelector).val() === stage.id.toString() ? 'visible' : 'none'
+        var layerIds = getLayerIds(stage.name),
+          visible = $(dropdownStageSelector).val() === stage.id.toString()
+            && mapType == 'zipcode';
+          standardLayer = {
+            "id": layerIds.zipcode,
+            "type": "fill",
+            "source": zipcodeSourceId,
+            "paint": App.MapsCommon.paintFill,
+            "layout": {
+              "visibility": visible ? 'visible' : 'none'
+            },
+            "filter": ["==", "stage_name", stage.name]
           },
-          "filter": ["==", "stage_name", stage.name]
-        };
-        hoverLayer = {
-          id: layerIds[2],
-          "type": "fill",
-          "source": zipcodeSourceId,
-          "paint": App.MapsCommon.hoverPaintFill,
-          "layout": {
-            "visibility": $(dropdownStageSelector).val() === stage.id.toString() ? 'visible' : 'none'
-          },
-          "filter": ["==", "zipcode", ""]
-        };
+          hoverLayer = {
+            id: layerIds.zipcodeHover,
+            "type": "fill",
+            "source": zipcodeSourceId,
+            "paint": App.MapsCommon.hoverPaintFill,
+            "layout": {
+              "visibility": visible ? 'visible' : 'none'
+            },
+            "filter": ["==", "zipcode", ""]
+          };
+        zipcodeLayers.push(standardLayer);
+        zipcodeLayers.push(hoverLayer);
         return {
           layers: [standardLayer, hoverLayer],
           hoverOffFilter: hoverLayer.filter,
@@ -118,7 +92,7 @@ $(document).on("turbolinks:load", function() {
       layers = addressLayers.concat(_.flatten(_.map(zipcodeLayersInfo, 'layers')));
       nonHoverLayers = _.map(addressLayers.concat(_.map(zipcodeLayersInfo, function(info) { return info.layers[0] })), 'id');
 
-      var map = new App.Map({
+      map = new App.Map({
         center: center,
         zoom: zoom,
         sources: sources,
@@ -128,20 +102,105 @@ $(document).on("turbolinks:load", function() {
       });
 
       if(map.loaded()) {
-        watchSelect(map, stages);
+        onMapLoad();
       } else {
         map.on('load', function() {
-          watchSelect(map, stages);
+          onMapLoad();
         });
       }
 
-      watchCardLink(stages);
+      watchCardLink();
     }
 
-    $.get({
-      url: "/candidates.geojson",
-      dataType: "json",
-      success: initMap.bind(this)
-    });
+    function getLayerIds(stage_name) {
+      return {
+        address: stage_name + "_address",
+        zipcode: stage_name + "_zipcode",
+        zipcodeHover: +stage_name + "_zipcode_hover"
+      };
+    }
+
+    function isCurrentStage(currentStageId) {
+      return function(stage) {
+        return stage.id.toString() === currentStageId;
+      };
+    }
+
+    function currentLayers() {
+      var isCurrentStageFunc = isCurrentStage(currentStageId),
+        currentStageName = stages.filter(isCurrentStageFunc)[0].name;
+      return getLayerIds(currentStageName);
+    }
+
+    function watchSelect() {
+      $(document).on("change", dropdownStageSelector, function(event) {
+        var currentStageId = this.value,
+          currentLayerIds,
+          unselectedStages;
+
+        currentLayerIds = currentLayers();
+        unselectedStages = _.reject(stages, isCurrentStage(currentStageId));
+
+        map.popup.remove();
+
+        _.each(unselectedStages, function(stage) {
+          if (mapType == 'address') {
+            map.setLayoutProperty(getLayerIds(stage).address, 'visibility', 'none');
+          } else {
+            map.setLayoutProperty(getLayerIds(stage).zipcode, 'visibility', 'none');
+            map.setLayoutProperty(getLayerIds(stage).zipcodeHover, 'visibility', 'none');
+          }
+        });
+        if (mapType == 'address') {
+          map.setLayoutProperty(currentLayerIds.address, 'visibility', 'visible');
+        } else {
+          map.setLayoutProperty(currentLayerIds.zipcode, 'visibility', 'visible');
+          map.setLayoutProperty(currentLayerIds.zipcodeHover, 'visibility', 'visible');
+        }
+      });
+    }
+
+    function watchMapType() {
+      $(".button-group.map-type").on("click", function(event) {
+        var $target = $(event.target),
+          currentLayersIds = currentLayers(),
+          addressVisibleValue,
+          zipcodeVisibleValue;
+        if (!$target.attr('selected')) {
+          $target.attr('selected', true)
+          $(".map-type i[data-map-type='" + mapType + "']").removeAttr('selected');
+          mapType = $target.attr('data-map-type');
+
+          addressVisibleValue = mapType == 'address'
+            ? 'visible'
+            : 'none';
+          zipcodeVisibleValue = mapType == 'zipcode'
+            ? 'visible'
+            : 'none';
+
+          map.setLayoutProperty(currentLayersIds.address, 'visibility', addressVisibleValue);
+          map.setLayoutProperty(currentLayersIds.zipcode, 'visibility', zipcodeVisibleValue);
+          map.setLayoutProperty(currentLayersIds.zipcodeHover, 'visibility', zipcodeVisibleValue);
+        }
+      });
+    }
+
+    function onMapLoad() {
+      watchMapType();
+      watchSelect();
+    }
+
+    function watchCardLink() {
+      $(document).on("click", ".maps-candidates .candidates-cards", function(event) {
+        var url = 
+          $(this).attr("href") 
+          + "?stage_name=" 
+          + stages.filter(isCurrentStage($(dropdownStageSelector).val()))[0].name;
+        event.preventDefault();
+        Turbolinks.visit(url);
+      });
+    }
+
+    init();
   }
 });
