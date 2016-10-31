4a95234995fcf3e4a5022792a6b3d772b08f1e83
johnwhelchel <john.l.whelchel@gmail.com>
2016-10-31 15:20:14 -0400

    Convert to S3 in production, hide map toggle with no addresses

--- lib/geo_json/state_data.rb
+++ lib/geo_json/state_data.rb
@@ -1,7 +1,6 @@
 class GeoJson::StateData
   MAPPING_FILE = 'lib/geo_json_data/state_to_zipcode_mapping.json'.freeze
   def initialize
-    @zipcode_json_by_state = {}
     @zipcode_mapping = JSON.parse(File.read("#{Rails.root}/#{MAPPING_FILE}"))
   end
 
@@ -11,27 +10,29 @@ class GeoJson::StateData
       raise(GeoJson::Zipcode::DataNotFoundError, "No state for #{zipcode}")
     end
 
-    unless zipcode_json_by_state.key?(state)
-      state_data = get_state_data(state)
-      zipcode_json_by_state[state] = JSON.parse(state_data)
-    end
-    zipcode_json_by_state[state]
+    state_data(state)
   end
 
   private
 
   attr_reader :zipcode_mapping
-  attr_accessor :zipcode_json_by_state
 
-  def get_state_data(state)
-    if Rails.env.development?
-      return File.read("#{Rails.root}/lib/geo_json_data/#{state}.json")
-    end
+  def state_data(state)
     cached_data = Rails.cache.read(cache_key(state))
     return cached_data if cached_data.present?
-    data = S3_BUCKET.object("geo_json_data/#{state}.json").get.body.read
-    Rails.cache.write(cache_key(state), data)
-    data
+    data = fetch_raw_data(state)
+    parsed_data = JSON.parse(data)
+    Rails.cache.write(cache_key(state), parsed_data)
+    parsed_data
+  end
+
+  def fetch_raw_data(state)
+    relative_file_path = "geo_json_data/#{state}.json"
+    if Rails.env.development?
+      File.read("#{Rails.root}/lib/#{relative_file_path}")
+    else
+      S3_BUCKET.object(relative_file_path).get.body.read
+    end
   end
 
   def state_for(zipcode)
--- spec/lib/aws_spec.rb
+++ spec/lib/aws_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe 'AWS' do
   describe '.object' do
     context 'success', vcr: { cassette_name: 'AWS-object-success' } do
       it 'works' do
-        data = S3_BUCKET.object("geo_json_data/state_to_zipcode_mapping.json").get
+        data = S3_BUCKET.object('geo_json_data/state_to_zipcode_mapping.json').get
         expect(data.present?).to be(true)
       end
     end
     context 'failure', vcr: { cassette_name: 'AWS-object-failure' } do
       it 'does not get non existent files' do
         expect {
-          S3_BUCKET.object("geo_json_data/i_do_not_exist.json").get
+          S3_BUCKET.object('geo_json_data/i_do_not_exist.json').get
         }.to raise_error(Aws::S3::Errors::AccessDenied)
       end
     end
