d41f35310f1a76c1b156071a4d436ddd9eb9be2c
johnwhelchel <john.l.whelchel@gmail.com>
2016-11-03 00:37:25 -0400

    Add tests for new controller

--- app/controllers/zipcode_controller.rb
+++ app/controllers/zipcode_controller.rb
@@ -3,7 +3,6 @@ class ZipcodeController < ApplicationController
   skip_before_action :block_invalid_subscriptions
 
   def geo_json
-    binding.pry
     skip_authorization
     expires_in 365.days, public: true
     response.headers['Content-Type'] = 'text/event-stream'
@@ -23,14 +22,12 @@ class ZipcodeController < ApplicationController
 
   def source_stream
     zipcode = params[:zipcode]
-    relative_file_path = "geo_json_data/zipcodes/"
     if Rails.env.development?
       File.open("#{Rails.root}/../geo_json_pre_convert_to_zip/zipcodes/#{zipcode}.json")
     else
-      S3_BUCKET.object(relative_file_path).get.body
+      S3_BUCKET.object("geo_json_data/zipcodes/#{zipcode}.json").get.body
     end
   rescue Errno::ENOENT, Aws::S3::Errors::AccessDenied => e
-    puts "COULDN'T FIND #{zipcode}"
     Logging::Logger.log("No zipcode data for #{zipcode}")
     nil
   end
