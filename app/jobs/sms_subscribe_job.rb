ac8754ccfb302daaf936770ca1528708ab27f88f
johnwhelchel <john.l.whelchel@gmail.com>
2016-11-10 11:36:38 -0500

    Add bang to make update throw

--- app/jobs/sms_subscribe_job.rb
+++ app/jobs/sms_subscribe_job.rb
@@ -2,7 +2,7 @@ class SmsSubscribeJob < ApplicationJob
   def perform(sender, message_sid)
     ApplicationRecord.transaction do
       MessageHandlerJob.perform_now(sender, message_sid)
-      sender.update(subscribed: true)
+      sender.update!(subscribed: true)
       sender.create_candidate unless sender.candidate
       AutomatonJob.perform_now(sender, 'subscribe')
     end
