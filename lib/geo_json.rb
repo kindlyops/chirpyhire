a05e62b849188493044bbaf344ed4f5a6af24c07
johnwhelchel <john.l.whelchel@gmail.com>
2016-11-11 09:22:23 -0500

    Quick fix for error with candidate with no phone number

--- lib/geo_json.rb
+++ lib/geo_json.rb
@@ -13,7 +13,7 @@ module GeoJson
 
   def self.single_description(candidate, candidate_location_p_tag: nil)
     "<h3>Candidate: <a href='/users/#{candidate.user_id}/messages'>\
-      #{candidate.phone_number.phony_formatted}</a></h3>\
+      #{candidate.phone_number&.phony_formatted}</a></h3>\
     <p class='handle sub-header'>#{candidate.handle}</p>
     #{candidate_location_p_tag}\
     <p>Created: #{time_ago_in_words(candidate.created_at)} ago</p>"
