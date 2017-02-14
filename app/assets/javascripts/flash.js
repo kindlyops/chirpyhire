465eb81f6f64d5d13cb72d686e994d5792820e45
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-14 14:20:36 -0500

    finishes reformatting devise forms

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,5 +1,5 @@
 $(document).on("turbolinks:load", function() {
   setTimeout(function() {
-    $('.flash-alert, .flash-success, .flash-error, .flash-notice').slideUp();
-  }, 3000);
+    $('.alert').slideUp();
+  }, 5000);
 });
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -314,3 +314,11 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
+
+Rails.application.config.to_prepare do
+  Devise::SessionsController.layout "devise"
+  Devise::RegistrationsController.layout proc{ |controller| account_signed_in? ? "application" : "devise" }
+  Devise::ConfirmationsController.layout "devise"
+  Devise::UnlocksController.layout "devise"
+  Devise::PasswordsController.layout "devise"
+end
