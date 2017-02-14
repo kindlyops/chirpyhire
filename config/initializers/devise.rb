9cc3d06668e6a87bb08e43fa35e4f804368867e8
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-14 14:28:01 -0500

    fix up flashes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -318,6 +318,7 @@ end
 Rails.application.config.to_prepare do
   Devise::SessionsController.layout "devise"
   Devise::RegistrationsController.layout proc{ |controller| account_signed_in? ? "application" : "devise" }
+  Devise::InvitationsController.layout proc{ |controller| account_signed_in? ? "application" : "devise" }
   Devise::ConfirmationsController.layout "devise"
   Devise::UnlocksController.layout "devise"
   Devise::PasswordsController.layout "devise"
