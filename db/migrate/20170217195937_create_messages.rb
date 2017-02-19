1f25d51f4871d1cff6140fb3d05609ded61c92b0
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-19 15:46:47 -0500

    do not require sent at or external created at

--- db/migrate/20170217195937_create_messages.rb
+++ db/migrate/20170217195937_create_messages.rb
@@ -4,8 +4,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
       t.string :sid, null: false
       t.text :body
       t.string :direction, null: false
-      t.datetime :sent_at, null: false
-      t.datetime :external_created_at, null: false
+      t.datetime :sent_at
+      t.datetime :external_created_at
       t.belongs_to :organization, null: false, index: true, foreign_key: true
       t.belongs_to :person, null: false, index: true, foreign_key: true
       t.timestamps
