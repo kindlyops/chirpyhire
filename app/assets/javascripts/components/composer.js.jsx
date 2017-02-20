ad884515893108d025abe6d271d6962cea71f6b3
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-20 16:14:43 -0500

    configures message form

--- app/assets/javascripts/components/composer.js.jsx
+++ app/assets/javascripts/components/composer.js.jsx
@@ -1,7 +1,7 @@
 var Composer = React.createClass({
   render: function() {
     return <li className='list-group-item write-message'>
-      <Forms.FormFor>
+      <Forms.FormFor url={'/subscribers/' + this.props.subscriberId} method='post' name='message'>
         <div className='form-group'>
           <textarea className='form-control' autoFocus='true' rows='1' placeholder='Type a message'></textarea>
           <button className='btn btn-primary' type='submit'>
