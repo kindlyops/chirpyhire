ce64c816007aa7dfc31d141e16af6cccc549644e
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-20 20:12:07 -0500

    adds empty state to messenger

--- app/decorators/conversation_decorator.rb
+++ app/decorators/conversation_decorator.rb
@@ -1,6 +1,17 @@
 class ConversationDecorator < Draper::Decorator
   delegate_all
 
+  def last_reply
+    return 'The conversation is just beginning!' if messages.empty?
+    "Last reply #{conversation.last_reply_at}"
+  end
+
+  def empty_state_message
+    'To start the conversation, type your message in the box below '\
+    "and then click 'Message'. We hope #{conversation.person_handle} "\
+    'is a great fit!'
+  end
+
   def last_reply_at
     return "at #{last_reply_at_clock_time}" if recently_replied?
 
