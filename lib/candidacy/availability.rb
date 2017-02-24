5b73ed7d118e830e6888a479bf5ba089522e9f6e
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-23 22:02:11 -0500

    consistent order of attributes

--- lib/candidacy/availability.rb
+++ lib/candidacy/availability.rb
@@ -1,8 +1,8 @@
 class Candidacy::Availability < Candidacy::Attribute
   def humanize_attributes
     {
-      hourly: 'Hourly',
       live_in: 'Live-In',
+      hourly: 'Hourly',
       both: 'Both',
       no_availability: 'None'
     }.with_indifferent_access
@@ -10,8 +10,8 @@ class Candidacy::Availability < Candidacy::Attribute
 
   def icon_classes
     {
-      hourly: 'fa-clock-o',
       live_in: 'fa-home',
+      hourly: 'fa-clock-o',
       both: 'fa-hourglass-half',
       no_availability: 'fa-hourglass-o'
     }.with_indifferent_access
