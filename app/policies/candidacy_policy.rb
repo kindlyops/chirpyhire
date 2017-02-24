d940a09ba6906066e86e9247dc70bef65a3e7354
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-23 21:46:38 -0500

    updates candidacy specs

--- app/policies/candidacy_policy.rb
+++ app/policies/candidacy_policy.rb
@@ -2,8 +2,8 @@ class CandidacyPolicy < ApplicationPolicy
   class Scope < ApplicationPolicy::Scope
     def resolve
       scope.finished_survey
-        .joins(person: :contacts)
-        .where(people: { contacts: { organization_id: organization.id } })
+           .joins(person: :contacts)
+           .where(people: { contacts: { organization_id: organization.id } })
     end
   end
 end
