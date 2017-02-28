6042809ad95a10d02c0d74ad81c98a74d197ec19
Harry Whelchel <harrywhelchel@gmail.com>
2017-02-28 18:57:41 -0500

    dry up messages channel

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,9 +1,14 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    contact = contacts.find(params[:contact_id])
     reject unless contact.present?
     stream_for contact
   end
 
   delegate :contacts, to: :current_organization
+
+  private
+
+  def contact
+    @contact ||= contacts.find(params[:contact_id])
+  end
 end
