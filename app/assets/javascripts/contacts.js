f89d47ef0c4cbad3d86fbe7dc448819b80a92a8d
Harry Whelchel <harrywhelchel@gmail.com>
2017-03-02 11:01:37 -0500

    completes high fidelity mockup of not ready to call list

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -29,9 +29,82 @@ $(document).on('turbolinks:load', function() {
           field: 'select',
           checkbox: true
         },{
-          field: 'person',
-          title: 'Candidate',
-          sortable: true
+          field: 'nickname',
+          title: 'Nickname (internal)',
+          sortable: true,
+          cellStyle: function(value, row, index, field) {
+            return {
+              classes: 'text-center'
+            };
+          }
+        },{
+          field: 'first_activity',
+          title: 'First Activity',
+          sortable: true,
+          cellStyle: function(value, row, index, field) {
+            return {
+              classes: 'text-center'
+            };
+          }
+        },{
+          field: 'survey_progress',
+          title: 'Survey Progress',
+          sortable: true,
+          cellStyle: function(value, row, index, field) {
+            return {
+              classes: 'text-center'
+            };
+          },
+          formatter: function(value, row, index) {
+            return ['<div class="progress">', '<div data-toggle="tooltip" ',
+            'title="75%" data-placement="right" class="progress-bar',
+            ' progress-bar-striped bg-warning" ',
+            'role="progressbar" aria-valuenow="75" aria-valuemin="0" ',
+            'aria-valuemax="100" style="width: 75%;"></div></div>'].join('');
+          }
+        },{
+          field: 'last_activity',
+          title: 'Last Activity',
+          sortable: true,
+          cellStyle: function(value, row, index, field) {
+            return {
+              classes: 'text-center'
+            };
+          }
+        },{
+          field: 'temperature',
+          title: 'Temperature',
+          sortable: true,
+          cellStyle: function(value, row, index, field) {
+            return {
+              classes: 'text-center temperature'
+            };
+          },
+          formatter: function(value, row, index) {
+            return ['<span class="badge badge-danger">Hot ',
+                    '<i class="fa fa-thermometer-full"></i>',
+                    '</span>'].join('');
+          }
+        },{
+          field: 'message',
+          title: 'Message',
+          cellStyle: function(value, row, index, field) {
+            return {
+              classes: 'text-center message-caregiver'
+            };
+          },
+          formatter: function(value, row, index) {
+            return ['<a class="btn btn-primary btn-block" href="/#">',
+                    '<i class="fa fa-commenting-o"></i>','</a>'].join('');
+          }
+        }],
+      data: [{
+        nickname: 'Witty Ox',
+        first_activity: '2/12/17 3:15pm',
+        survey_progress: '75%',
+        last_activity: '2/14/17 5:15pm',
+        temperature: 'Hot',
+        message: 'Message Me!'
       }]
     });
 
--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -2,4 +2,10 @@ $(document).on('turbolinks:load', function() {
   var toolTips = $('[data-toggle="tooltip"]:not([loaded])');
   toolTips.tooltip();
   toolTips.attr('loaded', true);
-})
+});
+
+$(document).on('load-success.bs.table', function() {
+  var toolTips = $('[data-toggle="tooltip"]:not([loaded])');
+  toolTips.tooltip();
+  toolTips.attr('loaded', true);
+});
