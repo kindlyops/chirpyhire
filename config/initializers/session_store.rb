3ebecc0ebaf78b3e7462a0921d9bc4900f46ab5d
Harry Whelchel <harry@standardcraft.io>
2017-03-03 18:58:29 -0500

    remove subdomain shared session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store,
-                                       key: '_congaree_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_congaree_session'
