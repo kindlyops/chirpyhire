4b9ae2a0c94bfd95f4d19209bcad3ec8c570b81b
Harry Whelchel <harry@standardcraft.io>
2017-03-03 19:26:38 -0500

    rename session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_congaree_session'
+Rails.application.config.session_store :cookie_store, key: '_chirpyhire_session'
