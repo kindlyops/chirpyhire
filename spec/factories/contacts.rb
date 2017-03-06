597fb97ba4bbf4ca6e9ee17ac5d0b77ef33831a1
Harry Whelchel <harrywhelchel@gmail.com>
2017-03-06 13:44:44 -0500

    adds another trait to contacts

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -3,6 +3,18 @@ FactoryGirl.define do
     person
     organization
 
+    trait :with_incomplete_candidacy do
+      after(:create) do |contact|
+        contact.person.candidacy.update(
+          contact: contact,
+          experience: Candidacy.experiences.keys.sample,
+          availability: Candidacy.availabilities.keys.sample,
+          skin_test: [true, false].sample,
+          zipcode: %w(30342 22902 30327 22903 90210).sample
+        )
+      end
+    end
+
     trait :with_complete_candidacy do
       after(:create) do |contact|
         contact.update(
