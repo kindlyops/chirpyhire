d9b7c7c69113fafcc4d445bf5c76d5369c19cea8
Harry Whelchel <harrywhelchel@gmail.com>
2017-03-09 13:44:14 -0500

    wip seeder

--- lib/seeder.rb
+++ lib/seeder.rb
@@ -2,7 +2,9 @@ class Seeder
   def seed
     seed_plan
     seed_organization
-    seed_dev_account
+    seed_account
+    seed_incomplete_contacts
+    seed_complete_contacts
   end
 
   private
@@ -26,12 +28,26 @@ class Seeder
 
   def seed_organization
     @organization = find_or_create_organization
-    FactoryGirl.create(:location, organization: organization)
+    seed_location
     organization.create_subscription(plan: plan, trial_message_limit: 1000)
     organization.create_ideal_candidate!(
-      zipcodes_attributes: [{ value: '30342' }]
+      zipcodes_attributes: [{ value: '30341' }]
     )
     puts 'Created Organization'
+  end
+
+  def seed_location
+    organization.location || organization.create_location(
+      latitude: 33.912779, longitude: -84.2975454,
+      full_street_address: full_street_address,
+      city: 'Chamblee', state: 'GA', state_code: 'GA',
+      postal_code: '30341', country: 'United States of America',
+      country_code: 'us'
+    )
+  end
+
+  def full_street_address
+    '2225 Spring Walk Court, Chamblee, GA 30341, United States of America'
   end
 
   def find_or_create_organization
@@ -39,16 +55,15 @@ class Seeder
       name: 'Happy Home Care',
       twilio_account_sid: ENV.fetch('TWILIO_ACCOUNT_SID'),
       twilio_auth_token: ENV.fetch('TWILIO_AUTH_TOKEN'),
-      phone_number: ENV.fetch('TEST_ORG_PHONE'),
-      stripe_customer_id: ENV.fetch('TEST_STRIPE_CUSTOMER_ID')
+      phone_number: ENV.fetch('DEMO_ORG_PHONE')
     )
   end
 
-  def seed_dev_account
+  def seed_account
     unless organization.accounts.present?
       organization.accounts.create!(
-        password: 'password',
-        email: ENV.fetch('DEV_EMAIL'),
+        password: ENV.fetch('DEMO_PASSWORD'),
+        email: ENV.fetch('DEMO_EMAIL'),
         super_admin: true
       )
     end
