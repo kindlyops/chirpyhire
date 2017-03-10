01f0f78a1dcaf995097c4a3a789ce1699b0c5e26
Harry Whelchel <harrywhelchel@gmail.com>
2017-03-10 16:34:08 -0500

    rubocop

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,7 +8,7 @@ class Account < ApplicationRecord
   has_attached_file :avatar,
                     styles: { medium: '300x300#', thumb: '100x100#' },
                     default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
 
   validates :email, uniqueness: true
 
