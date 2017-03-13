b546379b81c4d169c0344c340ceb1a00fbf6bbed
Harry Whelchel <harrywhelchel@gmail.com>
2017-03-13 11:13:45 -0400

    fixes copy to clipboard bug

--- app/assets/javascripts/copy_to_clipboard.js
+++ app/assets/javascripts/copy_to_clipboard.js
@@ -1,32 +1,38 @@
-$(document).on('ready', function() {
-  $('.clipboard').tooltip({
-    trigger: 'click',
-    placement: 'right'
-  });
+$(document).on('turbolinks:load', function() {
+  var unloadedClipboard = $('.clipboard:not([loaded])');
 
-  function setTooltip(btn, message) {
-    $(btn).tooltip('hide')
-      .attr('data-original-title', message)
-      .tooltip('show');
+  if (unloadedClipboard.length) {
+    unloadedClipboard.tooltip({
+      trigger: 'click',
+      placement: 'right'
+    });
+
+    function setTooltip(btn, message) {
+      $(btn).tooltip('hide')
+        .attr('data-original-title', message)
+        .tooltip('show');
+    }
+
+    function hideTooltip(btn) {
+      setTimeout(function() {
+        $(btn).tooltip('hide');
+      }, 1000);
+    }
+
+    var clipboard = new Clipboard('.clipboard');
+
+    clipboard.on('success', function(e) {
+      e.clearSelection();
+      setTooltip(e.trigger, 'Copied!');
+      hideTooltip(e.trigger);
+    });
+
+    clipboard.on('error', function(e) {
+      e.clearSelection();
+      setTooltip(e.trigger, 'Failed!');
+      hideTooltip(e.trigger);
+    });
+
+    unloadedClipboard.attr('loaded', true);
   }
-
-  function hideTooltip(btn) {
-    setTimeout(function() {
-      $(btn).tooltip('hide');
-    }, 1000);
-  }
-
-  var clipboard = new Clipboard('.clipboard');
-
-  clipboard.on('success', function(e) {
-    e.clearSelection();
-    setTooltip(e.trigger, 'Copied!');
-    hideTooltip(e.trigger);
-  });
-
-  clipboard.on('error', function(e) {
-    e.clearSelection();
-    setTooltip(e.trigger, 'Failed!');
-    hideTooltip(e.trigger);
-  });
 });
