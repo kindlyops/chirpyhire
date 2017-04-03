76bf793ef166ef24b048d4ad76a127a7868d0964
Harry Whelchel <harrywhelchel@gmail.com>
2017-04-03 11:48:01 -0400

    migrate to filterer class

--- lib/contact/filterer.rb
+++ lib/contact/filterer.rb
@@ -1,13 +1,38 @@
 class Contact::Filterer
   def initialize(params)
     @params = params
-    @filters = filter_options.select { |f| @params[f].present? }
-    @case_clause = 'CASE'
   end
 
   def order
+    filters = filter_options.select { |f| params[f].present? }
     return { id: :asc } unless filters.present?
-    filters.count.times(&method(:build_order))
+
+    case_clause = 'CASE'
+    filters.count.times do |time|
+      when_fragment = ' WHEN ('
+      combinations = filters.combination(filters.count - time)
+      combinations.each_with_index do |combination, c_index|
+        combination.each_with_index do |filter, f_index|
+          last_filter = f_index == (combination.count - 1)
+          end_of_combination = last_filter && (c_index < combinations.count - 1)
+          end_of_filter = f_index < combination.count - 1
+
+          filter_fragment = " candidacies.#{filter}='#{filter_mapping(filter)}'"
+          filter_fragment << if end_of_filter
+                               ' AND'
+                             elsif end_of_combination
+                               ') OR ('
+                             else
+                               ") THEN #{time}"
+                             end
+
+          when_fragment << filter_fragment
+        end
+      end
+      end_fragment = " ELSE #{time + 1} END as match#{stabilizer}"
+      when_fragment << end_fragment if time == (filters.count - 1)
+      case_clause << when_fragment
+    end
     case_clause
   end
 
@@ -17,66 +42,14 @@ class Contact::Filterer
     [:zipcode, :availability, :certification, :experience, :transportation]
   end
 
-  def build_order(time)
-    case_clause << ' WHEN (' << build_when_fragment(time) << end_fragment(time)
-  end
-
-  def build_when_fragment(time)
-    possible_matches = combinations(time).each_with_object('')
-    possible_matches.with_index do |(combination, filter_fragment), c|
-      combination.each_with_index do |filter, f|
-        count = combination.count
-
-        filter_fragment << " candidacies.#{filter}='#{filter_mapping(filter)}'"
-        filter_fragment << filter_postlude(f, c, count, time)
-      end
-    end
-  end
-
   def filter_mapping(filter)
     return params[filter] if filter == :zipcode
     Candidacy.send(filter.to_s.pluralize)[params[filter]]
   end
 
-  def combinations(time)
-    filters.combination(filters.count - time)
-  end
-
-  def last_time?(time)
-    time == (filters.count - 1)
-  end
-
-  def filter_postlude(f, c, count, time)
-    if intermediate_filter?(f, count)
-      ' AND'
-    elsif intermediate_combination?(f, c, count)
-      ') OR ('
-    else
-      ") THEN #{time}"
-    end
-  end
-
-  def last_filter?(f, combination_count)
-    f == (combination_count - 1)
-  end
-
-  def intermediate_filter?(f, combination_count)
-    f < (combination_count - 1)
-  end
-
-  def intermediate_combination?(f, c, combination_count)
-    last_filter?(f, combination_count) && c < (combination_count - 1)
-  end
-
-  attr_reader :filters, :params
-  attr_accessor :case_clause
+  attr_reader :params
 
   def stabilizer
     ', contact.id ASC'
-  end
-
-  def end_fragment(time)
-    return '' unless last_time?(time)
-    " ELSE #{time + 1} END as match#{stabilizer}"
   end
 end
