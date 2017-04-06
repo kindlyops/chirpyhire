2dc49af8cf762bd26b1d0dd4e507938f5ba23a5a
Harry Whelchel <harrywhelchel@gmail.com>
2017-04-06 16:31:29 -0400

    adds basic autocomplete widget

---
+++ app/assets/javascripts/autocomplete.js
@@ -0,0 +1,34 @@
+$(document).on('turbolinks:load', function() {
+
+  var locationAutocomplete = $('#location-autocomplete:not([loaded])');
+
+  if(locationAutocomplete.length) {
+    $(document).on('click', '#location-autocomplete', function() {
+      $(this).addClass('expanded');
+      $(this).find('input').attr('placeholder', 'Search zipcode, city, county, or state');
+    });
+
+    $(document).on('focusout', '#location-autocomplete', function() {
+      $(this).removeClass('expanded');
+      $(this).find('input').attr('placeholder', 'Anywhere');
+    });
+
+    function initMap() {
+      var input = document.getElementById('location-autocomplete');
+      var options = {
+        componentRestrictions: { country: 'us' },
+        types: ['(regions)']
+      };
+
+      var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+      autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        $(input).val(place.name);
+      });
+    }
+
+    initMap();
+    locationAutocomplete.attr('loaded', true);
+  }
+});
