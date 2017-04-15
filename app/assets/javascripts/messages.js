18fba410aeb0bc202880dd9c0ea31fa37aceacc1
Harry Whelchel <harry@standardcraft.io>
2017-04-15 10:34:29 -0400

    wip js window resizer

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -4,8 +4,27 @@ $(document).on('turbolinks:load', function() {
 
   if(messages.length) {
     var resizeScroller = function() {
-      var height = window.innerHeight - 200;
-      $('#msgs_scroller_div').css('height', height + 'px');
+      var cached_wh = $(window).height();
+
+      var msgs_scroller_y = $(".client_body").offset().top;
+      var footer_outer_h = $(".footer").outerHeight();
+
+      var msgs_scroller_height = cached_wh - msgs_scroller_y - footer_outer_h;
+      $('#msgs_scroller_div').css('height', msgs_scroller_height);
+
+      var end_div = $("#end_div");
+      var end_display_padder = $("#end_display_padder");
+      end_div.css("height", "");
+      end_display_padder.css("height", "");
+      var end_display_div = $("#end_display_div");
+      var h = end_display_div.outerHeight();
+      var allowed_h;
+      allowed_h = $('#msgs_scroller_div')[0].scrollHeight - $('#msgs_div').outerHeight();
+      allowed_h -= 32;
+      if (allowed_h > h) {
+        end_display_padder.css("height", allowed_h - h);
+      }
+      end_div.height(allowed_h);
     }
 
     $(window).resize(resizeScroller);
