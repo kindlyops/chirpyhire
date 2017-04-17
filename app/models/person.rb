779e48e042c6b31fae51c629a39b651d87bbcd10
Harry Whelchel <harry@standardcraft.io>
2017-04-17 19:16:18 -0400

    only set nickname for people without names

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,7 +5,7 @@ class Person < ApplicationRecord
   has_many :messages
   belongs_to :zipcode, optional: true
 
-  before_create :add_nickname
+  before_validation :add_nickname
   after_save :set_search_content
 
   delegate :inquiry, :availability, :experience,
@@ -16,6 +16,9 @@ class Person < ApplicationRecord
                     styles: { medium: '300x300#', thumb: '100x100#' },
                     default_url: '/images/:style/missing.png'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
+
+  validates :name, presence: true, unless: :nickname_present?
+  validates :nickname, presence: true, unless: :name_present?
 
   def subscribed_to?(organization)
     contacts.where(organization: organization).exists?
@@ -36,7 +39,7 @@ class Person < ApplicationRecord
   private
 
   def add_nickname
-    return if name.present? || nickname.present?
+    return if persisted? || name.present? || nickname.present?
     self.nickname = Nickname::Generator.new(self).generate
   rescue Nickname::OutOfNicknames => e
     Rollbar.debug(e)
@@ -45,5 +48,13 @@ class Person < ApplicationRecord
 
   def set_search_content
     contacts.find_each(&:save)
+  end
+
+  def nickname_present?
+    nickname.present?
+  end
+
+  def name_present?
+    name.present?
   end
 end
