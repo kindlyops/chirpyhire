887b106f8fd88a331ba75d3b8ca3efedf089d59f
Harry Whelchel <harry@standardcraft.io>
2017-04-17 18:34:33 -0400

    add person paperclip avatar

--- app/models/person.rb
+++ app/models/person.rb
@@ -13,6 +13,11 @@ class Person < ApplicationRecord
            :certification, :skin_test, :cpr_first_aid, :ideal?, to: :candidacy
   delegate :zipcode, to: :candidacy, prefix: true
 
+  has_attached_file :avatar,
+                    styles: { medium: '300x300#', thumb: '100x100#' },
+                    default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
+
   def subscribed_to?(organization)
     contacts.where(organization: organization).exists?
   end
