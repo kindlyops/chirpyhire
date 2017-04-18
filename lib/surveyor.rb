57243adf0f6bc73a49383d2a7e09df5f00df5835
Harry Whelchel <harry@standardcraft.io>
2017-04-18 14:35:09 -0400

    fixes rubocop

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -32,7 +32,11 @@ class Surveyor
   end
 
   def send_message(message)
-    organization.message(sender: Chirpy.person, recipient: person, body: message)
+    organization.message(
+      sender: Chirpy.person,
+      recipient: person,
+      body: message
+    )
   end
 
   def update_candidacy(message)
