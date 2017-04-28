e0a49f8ba6e6c8f3a6ff02c9560d55239f9a3bb8
Harry Whelchel <harry@standardcraft.io>
2017-04-28 19:05:39 -0400

    drift for smooch

--- app/assets/javascripts/smooch.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:before-cache', function () {
-  Smooch._container && $(Smooch._container).detach();
-});
-
-$(document).on('turbolinks:render', function () {
-  Smooch._container && $('body').append(Smooch._container);
-});
