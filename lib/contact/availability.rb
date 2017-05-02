7431ac674c6316ba15a575f1afc6d58a6eebf7fd
Harry Whelchel <harry@standardcraft.io>
2017-05-02 17:30:58 -0400

    add back old contact availabilities

--- lib/contact/availability.rb
+++ lib/contact/availability.rb
@@ -2,6 +2,9 @@ class Contact::Availability < Contact::Attribute
   def self.humanize_attributes
     {
       live_in: 'Live-In',
+      hourly: 'Hourly',
+      both: 'Both',
+      no_availability: 'No Availability',
       hourly_am: 'Hourly - AM',
       hourly_pm: 'Hourly - PM',
       open: 'Open'
@@ -11,9 +14,12 @@ class Contact::Availability < Contact::Attribute
   def self.icon_classes
     {
       live_in: 'fa-home',
+      hourly: 'fa-clock-o',
+      both: 'fa-hourglass-half',
+      no_availability: 'fa-hourglass-o',
       hourly_am: 'fa-sun-o',
       hourly_pm: 'fa-moon-o',
-      no_availability: 'fa-flag-checkered'
+      open: 'fa-flag-checkered'
     }.with_indifferent_access
   end
 end
