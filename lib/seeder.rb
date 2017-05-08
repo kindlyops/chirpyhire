13d0827191e4ada56fe2854414fe6e0bd4b76387
Harry Whelchel <harry@standardcraft.io>
2017-05-08 16:21:12 -0400

    adds broker to seeder

--- lib/seeder.rb
+++ lib/seeder.rb
@@ -1,5 +1,6 @@
 class Seeder
   def seed
+    seed_broker
     seed_organization
     seed_account
     seed_incomplete_contacts
@@ -144,6 +145,11 @@ class Seeder
     )
   end
 
+  def seed_broker
+    Broker.find_or_create_by!(broker_attributes)
+    puts 'Created Broker'
+  end
+
   def seed_organization
     @organization = find_or_create_organization
     organization.create_ideal_candidate!(
@@ -173,6 +179,14 @@ class Seeder
   def organization_attributes
     {
       name: ENV.fetch('DEMO_ORGANIZATION_NAME'),
+      twilio_account_sid: ENV.fetch('DEMO_TWILIO_ACCOUNT_SID'),
+      twilio_auth_token: ENV.fetch('DEMO_TWILIO_AUTH_TOKEN'),
+      phone_number: ENV.fetch('DEMO_ORGANIZATION_PHONE')
+    }
+  end
+
+  def broker_attributes
+    {
       twilio_account_sid: ENV.fetch('DEMO_TWILIO_ACCOUNT_SID'),
       twilio_auth_token: ENV.fetch('DEMO_TWILIO_AUTH_TOKEN'),
       phone_number: ENV.fetch('DEMO_ORGANIZATION_PHONE')
