c156c940a68527fab879988b6a8561a20911b90d
Harry Whelchel <harrywhelchel@gmail.com>
2017-05-14 13:59:55 -0400

    adds new team notification job

---
+++ app/jobs/new_team_notification_job.rb
@@ -0,0 +1,26 @@
+class NewTeamNotificationJob < ApplicationJob
+  def perform(account)
+    @account = account
+    @organization = account.organization
+
+    notifier.ping new_team_message # if Rails.env.production?
+  end
+
+  def notifier
+    @notifier ||= Slack::Notifier.new(
+      ENV.fetch('SLACK_WEBHOOK'), channel: '#general', username: 'freddy'
+    )
+  end
+
+  def new_team_message
+    <<~MESSAGE
+      New Team Signed Up!
+
+      Team: #{organization.name}
+      Owner Name: #{account.name}
+      Owner Email: #{account.email}
+    MESSAGE
+  end
+
+  attr_reader :account, :organization
+end
