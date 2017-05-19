ad58c706d4a4993dbc216c995248a5bb05ca7acf
Harry Whelchel <harry@standardcraft.io>
2017-05-19 10:39:29 -0400

    adds some contact migration specs

--- spec/lib/migrator/team_migrator_spec.rb
+++ spec/lib/migrator/team_migrator_spec.rb
@@ -46,16 +46,36 @@ RSpec.describe Migrator::TeamMigrator do
       expect {
         subject.migrate
       }.to change { Team.count }.by(1)
-      expect(Team.last.name).to eq(team.name)
+    end
+
+    it 'has the old team phone number' do
+      subject.migrate
       expect(Team.last.phone_number).to eq(phone_number)
+    end
+
+    it 'is tied to the new organization' do
+      subject.migrate
       expect(Team.last.organization).to eq(to_organization)
+    end
+
+    it 'has the old team name' do
+      subject.migrate
+      expect(Team.last.name).to eq(team.name)
     end
 
     it 'creates a new recruiting ad' do
       expect {
         subject.migrate
       }.to change { RecruitingAd.count }.by(1)
+    end
+
+    it 'has the old ad body' do
+      subject.migrate
       expect(RecruitingAd.last.body).to eq(team.recruiting_ad.body)
+    end
+
+    it 'is tied to the new organization' do
+      subject.migrate
       expect(RecruitingAd.last.organization).to eq(to_organization)
     end
 
@@ -65,6 +85,40 @@ RSpec.describe Migrator::TeamMigrator do
       accounts.map { |a| a[:to] }.each do |account|
         expect(Team.last.accounts).to include(account)
       end
-    end  
+    end
+
+    context 'with contact' do
+      let!(:contact) { create(:contact, team: team) }
+
+      context 'with conversations' do
+        before do
+          IceBreaker.call(contact)
+        end
+
+        it 'creates a new contact' do
+          expect {
+            subject.migrate
+          }.to change { Contact.count }.by(1)
+        end
+
+        it 'is tied to the old person' do
+          subject.migrate
+          expect(Contact.last.person).to eq(contact.person)
+        end
+
+        it 'is tied to the new team' do
+          subject.migrate
+          expect(Contact.last.team).to eq(Team.last)
+        end
+
+        context 'with read receipts' do
+
+        end
+      end
+
+      context 'with notes' do
+
+      end
+    end
   end
 end
