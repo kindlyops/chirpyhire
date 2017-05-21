9875e1ce472a6021e34678f230c11410d68f2ab1
Harry Whelchel <harrywhelchel@gmail.com>
2017-05-21 10:12:46 -0400

    updates devise mailer layouts

--- spec/mailers/previews/custom_devise_mailer_preview.rb
+++ spec/mailers/previews/custom_devise_mailer_preview.rb
@@ -2,4 +2,12 @@ class CustomDeviseMailerPreview < ActionMailer::Preview
   def invitation_instructions
     CustomDeviseMailer.invitation_instructions(Account.last, 'faketoken')
   end
+
+  def reset_password_instructions
+    CustomDeviseMailer.reset_password_instructions(Account.last, 'faketoken')
+  end
+
+  def password_change
+    CustomDeviseMailer.password_change(Account.last)
+  end
 end
