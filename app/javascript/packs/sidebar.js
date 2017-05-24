4bbb73deb83e90a407e6c23f329cbc2519b8d576
Harry Whelchel <harry@standardcraft.io>
2017-05-24 18:41:13 -0400

    high fidelity mockup of dropup coming along

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -1,8 +1,8 @@
 import Popper from 'popper.js';
 
 $(function() {
-  let reference = $('.agent-status-button')[0];
-  let popper = $('.agent-status-dropdown')[0];
+  let reference = $('.agent-status-button');
+  let popper = $('.agent-status-dropdown');
   
   new Popper(reference, popper, {
     placement: 'right'
