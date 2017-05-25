8dfcebf2deab29a1b350edb86cba5f6287abeb8c
Harry Whelchel <harry@standardcraft.io>
2017-05-24 20:48:51 -0400

    adds toggling dropup

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -1,15 +1,28 @@
 import Popper from 'popper.js';
 
 $(function() {
-  let reference = $('.agent-status-button');
-  let popper = $('.agent-status-dropdown');
-  
-  new Popper(reference, popper, {
-    placement: 'right',
-    modifiers: {
-      offset: {
-        offset: '-100px, 50%'
-      }
+  $(document).on('click', function(e) {
+    var withinDropdown = $(e.target).closest('.agent-status-dropdown').length;
+    var openingDropdown = $(e.target).closest('.agent-status-container').length;
+    if (!openingDropdown && !withinDropdown) {
+      $('body').removeClass('show-agent-status-dropdown');
+      Popper.destroy();
     }
+  });
+
+  $(document).on('click', '.agent-status-container', function(e) {
+    e.preventDefault();
+    $('body').addClass('show-agent-status-dropdown');
+    let reference = $('.agent-status-container');
+    let popper = $('.agent-status-dropdown');
+    
+    new Popper(reference, popper, {
+      placement: 'right',
+      modifiers: {
+        offset: {
+          offset: '-100px, 50%'
+        }
+      }
+    });
   });
 });
