b24d53aafdc4c976e6f068e73795c7bb317c418e
Harry Whelchel <harry@standardcraft.io>
2017-05-24 22:08:54 -0400

    makes bio a text field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523004817) do
+ActiveRecord::Schema.define(version: 20170525012929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170523004817) do
     t.integer "person_id", null: false
     t.integer "unread_count", default: 0, null: false
     t.integer "role", default: 0, null: false
+    t.text "bio"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["invitation_token"], name: "index_accounts_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_accounts_on_invitations_count"
