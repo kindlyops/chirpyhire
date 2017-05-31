c5cac85ed29e70443485d5aeb514349f721b3d37
Harry Whelchel <harry@chirpyhire.com>
2017-05-31 19:21:45 -0400

    update inbox builder

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -8,14 +8,14 @@ class Inbox < ApplicationRecord
   end
 
   def to_builder
-    conversations = inbox_conversations.by_recent_message.limit(25).map do |c|
-      c.to_builder.attributes!
+    conversations_json = conversations.by_recent_message.limit(25).map do |c|
+      c.inbox_conversations.find_by(inbox: self).to_builder.attributes!
     end
 
     Jbuilder.new do |json|
       json.id id
-      json.conversations_count inbox_conversations.count
-      json.conversations conversations
+      json.conversations_count conversations.count
+      json.conversations conversations_json
     end
   end
 end
