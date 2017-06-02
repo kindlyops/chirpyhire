707e62037db03382650d2444d9509ef24f5322e7
Harry Whelchel <harry@chirpyhire.com>
2017-06-01 22:51:24 -0400

    fix conversation race condition

--- app/controllers/teams/messages_controller.rb
+++ app/controllers/teams/messages_controller.rb
@@ -32,6 +32,7 @@ class Teams::MessagesController < ActionController::Base
   def create_subscribed_contact
     person.contacts.create(team: team).tap do |contact|
       contact.subscribe
+      contact.conversation
       IceBreakerJob.perform_later(contact)
     end
   end
