d9d598dda5240f7bf7d0b9d04238cb5a35ea4a49
Harry Whelchel <harry@standardcraft.io>
2017-06-20 14:23:27 -0400

    updates recruiting ad line breaks

--- app/models/recruiting_ad.rb
+++ app/models/recruiting_ad.rb
@@ -10,20 +10,15 @@ class RecruitingAd < ApplicationRecord
 
   def self.body(team)
     <<~BODY
-      #{team.organization_name} is hiring Caregivers! Our CNAs are as important to us as \
-      our clients. Openings PT/FT for M-F day shifts and weekends.
+      #{team.organization_name} is hiring Caregivers! Our CNAs are as important to us as our clients. Openings PT/FT for M-F day shifts and weekends.
 
       *********************************
 
-      For immediate opportunities, text START to \
-      #{team.phone_number.phony_formatted}
+      For immediate opportunities, text START to #{team.phone_number.phony_formatted}.
 
       *********************************
 
-      If you are passionate about helping the elderly remain safe and comfortable \
-      at home, enjoy working independently and are seeking a personal challenge, \
-      contact us to discuss our competitive wages, continuing education program and \
-      flexible scheduling opportunities.
+      If you are passionate about helping the elderly remain safe and comfortable at home, enjoy working independently and are seeking a personal challenge, contact us to discuss our competitive wages, continuing education program and flexible scheduling opportunities.
     BODY
   end
 end
