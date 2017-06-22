b2aeecaeb83786205bc157244e77098f499aacf9
Harry Whelchel <harry@chirpyhire.com>
2017-06-21 22:52:09 -0400

    clean up schema

---
+++ db/migrate/20170622024749_cleanup_schema.rb
@@ -0,0 +1,9 @@
+class CleanupSchema < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :read_receipts, :inbox_conversation_id
+    change_column_null :read_receipts, :conversation_id, false
+    drop_table :inbox_conversations
+    remove_column :inboxes, :account_id
+    change_column_null :inboxes, :team_id, false
+  end
+end
