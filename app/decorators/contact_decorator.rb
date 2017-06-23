09a09f6034eaa0c85d676e450a927f4fa6f799d3
Harry Whelchel <harry@chirpyhire.com>
2017-06-23 12:01:55 -0400

    removes unused helpers

--- app/decorators/contact_decorator.rb
+++ app/decorators/contact_decorator.rb
@@ -3,13 +3,6 @@ class ContactDecorator < Draper::Decorator
   decorates_association :person
   delegate :hero_pattern_classes, :handle, :phone_number, to: :person
 
-  def broad_query_params
-    {
-      zipcode: candidacy_zipcode.humanize_attribute,
-      certification: [certification.query].compact
-    }.delete_if { |_, v| v.blank? }
-  end
-
   def near_query_params
     near_params.delete_if { |_, v| v.blank? }
   end
@@ -22,55 +15,7 @@ class ContactDecorator < Draper::Decorator
     Contact::LastActiveAt.new(object)
   end
 
-  def availability
-    Contact::Availability.new(object)
-  end
-
-  def certification
-    Contact::Certification.new(object)
-  end
-
-  def cpr_first_aid
-    Contact::CprFirstAid.new(object)
-  end
-
-  def experience
-    Contact::Experience.new(object)
-  end
-
-  def live_in
-    Contact::LiveIn.new(object)
-  end
-
-  def skin_test
-    Contact::SkinTest.new(object)
-  end
-
-  def stats
-    Contact::Stats.new(object)
-  end
-
-  def transportation
-    Contact::Transportation.new(object)
-  end
-
   def candidacy_zipcode
     Contact::ZipCode.new(object)
-  end
-
-  def drivers_license
-    Contact::DriversLicense.new(object)
-  end
-
-  private
-
-  def near_params
-    broad_query_params.merge(transportation: [transportation.query].compact,
-                             availability: availabilities.compact,
-                             experience: [experience.query].compact)
-  end
-
-  def availabilities
-    availability.query_array.push(live_in.query)
   end
 end
