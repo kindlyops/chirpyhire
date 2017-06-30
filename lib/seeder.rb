ff64c726b729aff3806f06bb93f6b7bc50353725
Harry Whelchel <harry@chirpyhire.com>
2017-06-29 21:21:23 -0400

    adds default bot to seeder

--- lib/seeder.rb
+++ lib/seeder.rb
@@ -1,6 +1,7 @@
 class Seeder
   def seed
     account = Seeder::SeedAccount.call
+    BotMaker::DefaultBot.call(account.organization)
     Seeder::SeedContacts.call(account)
     Seeder::SeedMetrics.call(account)
   end
