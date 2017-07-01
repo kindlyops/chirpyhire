995e2f707dce5a530cb61571045f17a0e88e1a2a
Harry Whelchel <harry@chirpyhire.com>
2017-06-30 20:18:57 -0400

    add phone number query

--- lib/person/phone_number_attribute.rb
+++ lib/person/phone_number_attribute.rb
@@ -3,6 +3,10 @@ class Person::PhoneNumberAttribute < Person::Attribute
     person.phone_number.phony_formatted
   end
 
+  def query
+    :phone_number
+  end
+
   def icon_class
     return 'fa-question' if person.phone_number.blank?
 
