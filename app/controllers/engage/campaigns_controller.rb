f2e0e8e05ea93fb53cd1489c69a7ece8e1b665a7
Harry Whelchel <harry@chirpyhire.com>
2017-07-05 17:44:24 -0400

    adds engage navigation component

---
+++ app/controllers/engage/campaigns_controller.rb
@@ -0,0 +1,20 @@
+class Engage::CampaignsController < ApplicationController
+  layout 'react'
+
+  def new
+    @campaign = authorize(current_organization.campaigns.build)
+
+    respond_to do |format|
+      format.html { render html: '', layout: true }
+    end
+  end
+  
+  def show
+    @campaign = authorize(Campaign.find(params[:id]))
+
+    respond_to do |format|
+      format.json
+      format.html { render html: '', layout: true }
+    end
+  end
+end
--- app/javascript/engage/components/engageNavigation.jsx
+++ app/javascript/engage/components/engageNavigation.jsx
@@ -17,15 +17,41 @@ class EngageNavigation extends React.Component {
         <div className='ch--vertical-navigation--inner'>
           <div className='ch--vertical-navigation-header'>
             <h3 className='small-uppercase'>Campaigns</h3>
-            {this.state.campaigns.map(campaign => 
-              <NavLink key={campaign.id} to={`/engage/campaigns/${campaign.id}`}>{campaign.name}</NavLink>
-            )}
           </div>
+          <div className='ch--vertical-navigation-links'>
+            {this.state.campaigns.map(campaign => 
+              <NavLink 
+                className='ch--vertical-navigation-link' 
+                key={campaign.id} 
+                to={`/engage/campaigns/${campaign.id}`}>{campaign.name}</NavLink>
+            )}
+            <NavLink 
+              className='ch--vertical-navigation-link' 
+              to='/engage/campaigns/new' 
+              className='ch--vertical-navigation-link'>
+              <i className='fa fa-plus fa-fw mr-2'></i>
+              New Campaign
+            </NavLink>
+          </div>
+        </div>
+        <div className='ch--vertical-navigation--inner'>
           <div className='ch--vertical-navigation-header'>
             <h3 className='small-uppercase'>Recruitbots</h3>
+          </div>
+          <div className='ch--vertical-navigation-links'>
             {this.state.bots.map(bot => 
-              <NavLink key={bot.id} to={`/engage/bots/${bot.id}`}>{bot.name}</NavLink>
+              <NavLink 
+                className='ch--vertical-navigation-link' 
+                key={bot.id} 
+                to={`/engage/bots/${bot.id}`}>{bot.name}</NavLink>
             )}
+            <NavLink 
+              className='ch--vertical-navigation-link' 
+              to='/engage/bots/new' 
+              className='ch--vertical-navigation-link'>
+              <i className='fa fa-plus fa-fw mr-2'></i>
+              New Bot
+            </NavLink>
           </div>
         </div>
 
