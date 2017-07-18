49114ac165669e540a6c18dde2e86713586dd782
Harry Whelchel <harry@standardcraft.io>
2017-07-18 16:57:53 -0400

    adds name validation for campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -9,6 +9,9 @@ class Campaign < ApplicationRecord
   has_many :messages
   accepts_nested_attributes_for :bot_campaign
 
+  validates :name, uniqueness: true
+  validates :name, presence: true
+
   def self.recent
     order(created_at: :desc)
   end
