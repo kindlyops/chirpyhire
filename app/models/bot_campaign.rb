0eb21c7bbca9d314957bfffbe1644e3153e839b0
Harry Whelchel <harry@standardcraft.io>
2017-07-18 17:00:35 -0400

    use word survey

--- app/models/bot_campaign.rb
+++ app/models/bot_campaign.rb
@@ -3,5 +3,5 @@ class BotCampaign < ApplicationRecord
   belongs_to :campaign
   belongs_to :inbox
 
-  validates :inbox, uniqueness: { message: 'already has an active Recruitbot.' }
+  validates :inbox, uniqueness: { message: 'already has an active Survey.' }
 end
