4d55675ae6cd3fca1a7b3d6e6165e385fb855d9e
Harry Whelchel <harry@standardcraft.io>
2017-07-19 13:19:30 -0400

    sets up basic call forwarding

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,17 +1,28 @@
 class TwilioController < ActionController::Base
   protect_from_forgery with: :null_session
+  after_action :set_header
 
   def voice
-    render xml: twiml.to_xml
+    if phone_number.forwarding_phone_number.present?
+      render xml: forward_twiml.to_xml
+    else
+      render xml: default_twiml.to_xml
+    end
   end
 
   private
 
-  def twiml
-    @twiml ||= default_response
+  def phone_number
+    @phone_number ||= PhoneNumber.find_by(phone_number: params['To'])
   end
 
-  def default_response
+  def forward_twiml
+    Twilio::TwiML::Response.new do |response|
+      response.Dial phone_number.forwarding_phone_number
+    end
+  end
+
+  def default_twiml
     Twilio::TwiML::Response.new do |response|
       response.Pause length: 1
       response.Say sentence_one, voice: 'woman'
@@ -39,4 +50,10 @@ class TwilioController < ActionController::Base
     To reach us, text us back at this number. Have a fantastic day!
     response
   end
+
+  def set_header
+    response.headers['Content-Type'] = 'text/xml'
+  end
+
+  delegate :organization, to: :phone_number
 end
