e670d12eb6670ae729128882357fb5da319f24a3
Harry Whelchel <harry@standardcraft.io>
2017-07-25 15:32:10 -0400

    removes more code

--- app/jobs/already_subscribed_job.rb
+++
@@ -1,7 +0,0 @@
-class AlreadySubscribedJob < ApplicationJob
-  def perform(contact, message_sid)
-    message = MessageSyncer.call(contact, message_sid)
-
-    AlreadySubscribed.call(contact, message)
-  end
-end
