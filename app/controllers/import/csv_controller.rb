0bf1ffc10cfb13769906cf8170006eea31e01e49
Harry Whelchel <harry@chirpyhire.com>
2017-07-26 17:19:50 -0400

    adds beginning of mapping attributes page

--- app/controllers/import/csv_controller.rb
+++ app/controllers/import/csv_controller.rb
@@ -5,7 +5,25 @@ class Import::CsvController < ApplicationController
     @import = authorize new_import
   end
 
+  def create
+    @import = authorize build_import
+
+    if @import.save
+      redirect_to edit_import_csv_path(@import)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @import = authorize current_account.imports.find(params[:id])
+  end
+
   private
+
+  def build_import
+    current_account.imports.build(permitted_attributes(Import))
+  end
 
   def new_import
     current_account.imports.build
--- app/policies/import_policy.rb
+++ app/policies/import_policy.rb
@@ -3,6 +3,14 @@ class ImportPolicy < ApplicationPolicy
     record.new_record?
   end
 
+  def edit?
+    show?
+  end
+
+  def permitted_attributes
+    %i[file]
+  end
+
   class Scope < ApplicationPolicy::Scope
     def resolve
       scope.joins(:account).where(accounts: { organization: organization })
