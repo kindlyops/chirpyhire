c4190f18a3f1eeec89204a924ffec91773e45896
Harry Whelchel <harry@chirpyhire.com>
2017-07-27 18:47:51 -0400

    adds contacts imports scopes

--- app/models/contacts_import.rb
+++ app/models/contacts_import.rb
@@ -1,4 +1,12 @@
 class ContactsImport < ApplicationRecord
   belongs_to :contact
   belongs_to :import
+
+  def self.created
+    where(updated: false)
+  end
+
+  def self.updated
+    where(updated: true)
+  end
 end
