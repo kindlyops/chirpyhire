2f1b69b9803aa427cdd6cfbfbe1db774e67a715a
Harry Whelchel <harry@standardcraft.io>
2017-07-28 11:06:21 -0400

    set phone number on new contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
+  phony_normalize :phone_number, default_country_code: 'US'
   include PgSearch
   pg_search_scope :search_by_name,
                   against: { name: 'A', nickname: 'B' },
