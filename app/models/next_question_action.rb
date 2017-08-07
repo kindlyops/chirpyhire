855039c5ca1faa231b4a111e186383090f2ed432
Harry Whelchel <harry@standardcraft.io>
2017-08-07 17:57:59 -0400

    removes unused code

--- app/models/next_question_action.rb
+++ app/models/next_question_action.rb
@@ -4,21 +4,4 @@ class NextQuestionAction < BotAction
   def next_question?
     true
   end
-
-  def next_step
-    return first_goal if next_question.blank?
-  end
-
-  def trigger_next_question
-    return trigger_first_goal if next_question.blank?
-    next_question.trigger(message, campaign_contact)
-  end
-
-  def next_question
-    bot.question_after(campaign_contact.question)
-  end
-
-  def trigger_first_goal
-    first_goal.trigger(message, campaign_contact)
-  end
 end
