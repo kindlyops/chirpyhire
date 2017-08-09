92a173659776c8501431f2577f718616aa87cae5
Harry Whelchel <harry@standardcraft.io>
2017-08-09 16:00:25 -0400

    wip bot list view

---
+++ app/decorators/bot_decorator.rb
@@ -0,0 +1,11 @@
+class BotDecorator < Draper::Decorator
+  delegate_all
+
+  def created_at
+    TimestampDecorator.new(object, :created_at)
+  end
+
+  def last_edited_at
+    TimestampDecorator.new(object, :last_edited_at)
+  end
+end
