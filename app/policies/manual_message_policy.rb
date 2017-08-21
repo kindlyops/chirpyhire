03f8d58b7d7d5322d772f8c4605e46fdfad3db32
Harry Whelchel <harry@chirpyhire.com>
2017-08-21 18:28:48 -0400

    rubocop

--- app/policies/manual_message_policy.rb
+++ app/policies/manual_message_policy.rb
@@ -15,16 +15,16 @@ class ManualMessagePolicy < ApplicationPolicy
     %i[zipcode_default_city_eq zipcode_state_abbreviation_eq
        zipcode_county_name_eq zipcode_zipcode_eq
        last_reply_at_gt_days_ago last_reply_at_lt_days_ago
-       last_reply_at_eq_days_ago
-       created_at_gt_days_ago created_at_lt_days_ago
-       created_at_eq_days_ago
-       name_cont messages_count_eq].concat(
-         [
-           matches_all_tags: [],
-           matches_all_manual_messages: [],
-           contact_stage_id_in: []
-         ]
-       )
+       last_reply_at_eq_days_ago created_at_gt_days_ago created_at_lt_days_ago
+       created_at_eq_days_ago name_cont messages_count_eq].concat(array_params)
+  end
+
+  def array_params
+    [
+      matches_all_tags: [],
+      matches_all_manual_messages: [],
+      contact_stage_id_in: []
+    ]
   end
 
   class Scope < ApplicationPolicy::Scope
