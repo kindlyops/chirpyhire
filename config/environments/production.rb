a5e14ec5964b595529e2cea0525700636e9e33d1
Harry Whelchel <harry@chirpyhire.com>
2017-08-26 17:58:11 -0400

    adds asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['SMTP_HOST'] }
+  config.action_mailer.asset_host = "https://#{ENV['SMTP_HOST']}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
-
+  config.action_mailer.asset_host = 'localhost'
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
