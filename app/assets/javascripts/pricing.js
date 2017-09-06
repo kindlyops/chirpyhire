49f6f481cf4a2dc39030495bc536fa5e1f78b742
Harry Whelchel <harry@chirpyhire.com>
2017-09-05 20:23:01 -0400

    update pricing

--- app/assets/javascripts/pricing.js
+++ app/assets/javascripts/pricing.js
@@ -6,19 +6,40 @@ $(function() {
     if(candidatesCount <= 50) {
       $('.settings.pricing .candidates--price').text('$125 / mo');
     } else if(candidatesCount <= 100) {
-      $('.settings.pricing .candidates--price').text('$170 / mo');
-    } else if(candidatesCount <= 150) {
-      $('.settings.pricing .candidates--price').text('$195 / mo');
+      $('.settings.pricing .candidates--price').text('$150 / mo');
     } else if(candidatesCount <= 200) {
-      $('.settings.pricing .candidates--price').text('$220 / mo');
+      $('.settings.pricing .candidates--price').text('$175 / mo');
+    } else if(candidatesCount <= 300) {
+      $('.settings.pricing .candidates--price').text('$225 / mo');
+    } else if(candidatesCount <= 400) {
+      $('.settings.pricing .candidates--price').text('$250 / mo');
     } else if(candidatesCount <= 500) {
+      $('.settings.pricing .candidates--price').text('$275 / mo');
+    } else if(candidatesCount <= 600) {
+      $('.settings.pricing .candidates--price').text('$315 / mo');
+    } else if(candidatesCount <= 700) {
       $('.settings.pricing .candidates--price').text('$350 / mo');
+    } else if(candidatesCount <= 800) {
+      $('.settings.pricing .candidates--price').text('$380 / mo');
+    } else if(candidatesCount <= 900) {
+      $('.settings.pricing .candidates--price').text('$400 / mo');
     } else if(candidatesCount <= 1000) {
-      $('.settings.pricing .candidates--price').text('$475 / mo');
+      $('.settings.pricing .candidates--price').text('$450 / mo');
     } else if(candidatesCount <= 2000) {
-      $('.settings.pricing .candidates--price').text('$750 / mo');
-    } else if(candidatesCount <= 4000) {
-      $('.settings.pricing .candidates--price').text('$1,000 / mo');
+      var priceCount = Math.floor(candidatesCount / 100) * 100;
+      var price = priceCount * 0.43;
+      var priceString = '$' + Math.round(price) + ' / mo';
+      $('.settings.pricing .candidates--price').text(priceString);
+    } else if(candidatesCount <= 3000) {
+      var priceCount = Math.floor(candidatesCount / 100) * 100;
+      var price = priceCount * 0.42;
+      var priceString = '$' + Math.round(price) + ' / mo';
+      $('.settings.pricing .candidates--price').text(priceString);
+    } else {
+      var priceCount = Math.floor(candidatesCount / 100) * 100;
+      var price = priceCount * 0.41;
+      var priceString = '$' + Math.round(price) + ' / mo';
+      $('.settings.pricing .candidates--price').text(priceString);
     }
   });
 });
