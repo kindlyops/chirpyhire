e8a31020454a356a09a4f4928fbbe9f48996766a
Harry Whelchel <harry@chirpyhire.com>
2017-09-18 21:49:30 -0400

    removes errant delegation

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,6 @@ class Invoice < ApplicationRecord
   belongs_to :subscription, optional: true,
                             foreign_key: :subscription, primary_key: :stripe_id
   belongs_to :organization, foreign_key: :customer, primary_key: :stripe_id
-  delegate :organization, to: :subscription
 
   def lines
     (self[:lines]['data'] || []).map { |item| InvoiceItem.new(item) }
