c0036e09b594cbcff3a683fb623a72f6f8beb1d8
Harry Whelchel <harry@standardcraft.io>
2017-09-26 23:40:37 -0400

    Revert "updates webpack"
    This reverts commit 691d87862746e7e66af669231e44e213a80729dc.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,21 @@
-const environment = require('./environment')
+// Note: You must restart bin/webpack-dev-server for changes to take effect
 
-module.exports = environment.toWebpackConfig()
+/* eslint global-require: 0 */
+
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const CompressionPlugin = require('compression-webpack-plugin')
+const sharedConfig = require('./shared.js')
+
+module.exports = merge(sharedConfig, {
+  output: { filename: '[name]-[chunkhash].js' },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|svg|eot|ttf|woff|woff2)$/
+    })
+  ]
+})
