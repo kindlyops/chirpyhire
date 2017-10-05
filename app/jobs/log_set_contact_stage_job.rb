1f07401fd46227365822d5b878a8985d8a5218fb
Harry Whelchel <harry@chirpyhire.com>
2017-10-05 18:10:35 -0400

    removes more heap

--- app/jobs/log_set_contact_stage_job.rb
+++
@@ -1,5 +0,0 @@
-class LogSetContactStageJob < ApplicationJob
-  def perform(account, stage, contact, timestamp)
-    Internal::Logger::SetContactStage.call(account, stage, contact, timestamp)
-  end
-end
